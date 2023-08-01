3306a69001c126f9c30d447c49e2c22bc402b9e1
Udo Bauer <knackwurstking.tux@gmail.com>
2023-08-01 14:16:48 +0100

    set container class name

--- src/lib/components/infinite-scroll-view/InfiniteScrollView.svelte
+++ src/lib/components/infinite-scroll-view/InfiniteScrollView.svelte
@@ -2,3 +2,10 @@
 </script>
 
 <!-- TODO: tree containers (divs), visible view will always be the second one, on scroll(end) reorder divs -->
+<div class="infinite-scroll-view--container">
+</div>
+
+<style>
+    .infinite-scroll-view--container {
+    }
+</style>
