b908721102387b8f1cb3d2cb45e1a6a2acfa704c
Udo Bauer <knackwurstking.tux@gmail.com>
2023-08-01 13:23:09 +0200

    adding date picker preview container and span (text; year/month)

--- src/lib/components/date-picker/DatePicker.svelte
+++ src/lib/components/date-picker/DatePicker.svelte
@@ -4,3 +4,22 @@
 </script>
 
 <!-- TODO: Date picker for selecting the current month and year -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+  class="date-picker--preview"
+  role="button"
+  tabindex="0"
+  on:click={() => {
+    // TODO: open dialog for changing the current year and month in use
+  }}
+>
+  <span>{current.year || "YYYY"} / {(current.month || "MM").toString().padStart(2, "0")}</span>
+</div>
+
+<style>
+  div.date-picker--preview {
+  }
+
+  div.date-picker--preview span {
+  }
+</style>
