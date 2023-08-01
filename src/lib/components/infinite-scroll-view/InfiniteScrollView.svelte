4c1d4ca5b1670be6eb1c92b55941ad5f0ca80c10
Udo Bauer <knackwurstking.tux@gmail.com>
2023-08-01 21:06:34 +0100

    catch after, current and before scroll positions (@todo: reorder sections?)

--- src/lib/components/infinite-scroll-view/InfiniteScrollView.svelte
+++ src/lib/components/infinite-scroll-view/InfiniteScrollView.svelte
@@ -1,47 +1,81 @@
 <script>
+  /**
+   * @typedef Items
+   * @type {{
+   *  type: "before"  | "current" | "after" | "null"
+   * }}
+   */
+
+  /** @type {Items[]} */
+  let items = [{ type: "before" }, { type: "current" }, { type: "after" }];
 </script>
 
 <!-- TODO: tree containers (divs), visible view will always be the second one, on scroll(end) reorder divs -->
-<div>
-    <section></section>
-    <section></section>
-    <section></section>
+<div
+  on:scroll={(ev) => {
+    // NOTE: This won't work if a border is set for this div
+    let cs = window.getComputedStyle(ev.currentTarget);
+    const borderWidth = parseInt(cs.borderTopWidth, 10) + parseInt(cs.borderBottomWidth, 10);
+
+    cs = window.getComputedStyle(ev.currentTarget.children[0]);
+    const sectionMargin = parseInt(cs.marginTop, 10) + parseInt(cs.marginBottom, 10);
+
+    const rect = ev.currentTarget.getBoundingClientRect();
+
+    switch (ev.currentTarget.scrollTop) {
+        case 0:
+            console.debug("scroll position: before");
+            break;
+        case rect.height + sectionMargin - borderWidth:
+            console.debug("scroll position: current");
+            break
+        case (rect.height + sectionMargin - borderWidth) * 2:
+            console.debug("scroll position: after");
+            break
+    }
+  }}
+>
+  {#each items as item}
+    <section>{item.type}</section>
+  {/each}
 </div>
 
 <style>
-    div {
-        width: 100%;
-        height: 100%;
+  div {
+    width: 100%;
+    height: 100%;
 
-        display: flex;
-        flex-direction: column;
+    display: flex;
+    flex-direction: column;
 
-        overflow-y: scroll;
+    overflow-y: scroll;
 
-        scroll-snap-type: y mandatory;
+    scroll-snap-type: y mandatory;
 
-        border: 1px solid red;
-    }
+    /*
+    border: 1px solid red;
+    */
+  }
 
-    div > section {
-        width: 100%;
+  div > section {
+    width: 100%;
 
-        min-height: 100%;
-        height: 100%;
-        max-height: 100%;
+    min-height: 100%;
+    height: 100%;
+    max-height: 100%;
 
-        border: 1px solid yellow;
-    }
+    border: 1px solid yellow;
+  }
 
-    div > section:nth-child(1) {
-        scroll-snap-align: start;
-    }
+  div > section:nth-child(1) {
+    scroll-snap-align: start;
+  }
 
-    div > section:nth-child(2) {
-        scroll-snap-align: center;
-    }
+  div > section:nth-child(2) {
+    scroll-snap-align: center;
+  }
 
-    div > section:nth-child(3) {
-        scroll-snap-align: end;
-    }
+  div > section:nth-child(3) {
+    scroll-snap-align: end;
+  }
 </style>
