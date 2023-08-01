0fd3bf1c137f0ec9c962ead3c6b37845db422dcc
Udo Bauer <knackwurstking.tux@gmail.com>
2023-08-01 20:25:46 +0100

    replace item divs with section elements

--- src/lib/components/infinite-scroll-view/InfiniteScrollView.svelte
+++ src/lib/components/infinite-scroll-view/InfiniteScrollView.svelte
@@ -2,14 +2,46 @@
 </script>
 
 <!-- TODO: tree containers (divs), visible view will always be the second one, on scroll(end) reorder divs -->
-<div class="infinite-scroll-view--container">
+<div>
+    <section></section>
+    <section></section>
+    <section></section>
 </div>
 
 <style>
-    .infinite-scroll-view--container {
+    div {
         width: 100%;
         height: 100%;
 
+        display: flex;
+        flex-direction: column;
+
+        overflow-y: scroll;
+
+        scroll-snap-type: y mandatory;
+
         border: 1px solid red;
+    }
+
+    div > section {
+        width: 100%;
+
+        min-height: 100%;
+        height: 100%;
+        max-height: 100%;
+
+        border: 1px solid yellow;
+    }
+
+    div > section:nth-child(1) {
+        scroll-snap-align: start;
+    }
+
+    div > section:nth-child(2) {
+        scroll-snap-align: center;
+    }
+
+    div > section:nth-child(3) {
+        scroll-snap-align: end;
     }
 </style>
