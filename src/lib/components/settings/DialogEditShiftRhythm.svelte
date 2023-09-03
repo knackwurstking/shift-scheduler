335bd3e946e6f96559512666eeba40e135788985
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-03 15:39:52 +0200

    edit shift rhythm dialog positioning and sizing

--- src/lib/components/settings/DialogEditShiftRhythm.svelte
+++ src/lib/components/settings/DialogEditShiftRhythm.svelte
@@ -22,24 +22,60 @@
             {/each}
         </ul>
 
-        <ul>
+        <div class="shifts-container">
             <!-- TODO: this needs some different styling -->
             {#each shifts as shift, index}
-                <Shift
-                    {...shift}
-                    on:click={() => {
-                        rhythm = [
-                            ...rhythm,
-                            shift.name,
-                        ];
-                    }}
-                />
+                <div class="container">
+                    <Shift
+                        {...shift}
+                        on:click={() => {
+                            rhythm = [
+                                ...rhythm,
+                                shift.name,
+                            ];
+                        }}
+                    />
+                </div>
             {/each}
-        </ul>
+        </div>
 
         <button type="submit" on:click={() => dispatch("submit", rhythm)}>OK</button>
     </article>
 </dialog>
 
 <style>
+    article {
+        height: 90%;
+        width: calc(100% - var(--spacing));
+
+        overflow: hidden;
+    }
+
+    article ul {
+        height: calc(100% - 10rem);
+
+        overflow: hidden;
+        overflow-y: auto;
+
+        border-bottom: 1px solid var(--muted-border-color);
+
+        margin-bottom: 4px;
+    }
+
+    article .shifts-container {
+        overflow: hidden;
+        overflow-x: auto;
+
+        display: flex;
+
+        margin: 4px 0 8px 0;
+        padding: 0 0 8px 0;
+
+        border-bottom: 1px solid var(--muted-border-color);
+    }
+
+    article .shifts-container .container {
+        width: calc(4.5em + 16px);
+        margin: 4px;
+    }
 </style>
