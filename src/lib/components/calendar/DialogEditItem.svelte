d7828a963aead5bc5dfb21c8bd213556414b89cd
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-04 15:39:00 +0200

    add submit button with a dispatch

--- src/lib/components/calendar/DialogEditItem.svelte
+++ src/lib/components/calendar/DialogEditItem.svelte
@@ -7,4 +7,17 @@
 
     /** @type {Date} */
     export let date;
+
+    /** @type {ShiftItem | null} */
+    export let shift = null;
+    /** @type {string | null} */
+    export let note = null;
 </script>
+
+<dialog {open}>
+    <article>
+        <!-- TODO: ... -->
+
+        <button on:click={() => dispatch("submit", { default: null, shift, note })}>OK</button>
+    </article>
+</dialog>
