b27e6a6bb849aea4a07ca0c5f4f5184cd8983d0a
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-04 15:34:43 +0200

    EditItem dialog (@todo)

---
+++ src/lib/components/calendar/DialogEditItem.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    // TODO: notes and custom shift for the current grid item (date: "YYYY:MM:DD")
+    export let open = false;
+
+    /** @type {Date} */
+    export let date;
+</script>
