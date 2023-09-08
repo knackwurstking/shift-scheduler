260ac9452aec6b3985a2ebdea0bb0125103ec997
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-08 13:11:51 +0200

    create db objectStore: data

--- src/lib/js/db/index.js
+++ src/lib/js/db/index.js
@@ -1,4 +1,22 @@
 export const currentVersion = 1;
-export const db = indexedDB.open("data", currentVersion);
+export const db = indexedDB.open("db", currentVersion);
 
 // TODO: custom shifts and notes per day (primary key (date): "YYYY-MM-DD")
+
+db.onupgradeneeded = (ev) => {
+    switch (ev.oldVersion) {
+        case 0:
+            const result = db.result;
+            if (!result.objectStoreNames.contains("data")) {
+                result.createObjectStore("data", { keyPath: "date" });
+            }
+    }
+}
+
+db.onerror = () => {
+    console.error("DB Error:", db.error);
+}
+
+db.onsuccess = () => {
+    // TODO: working with db object
+}
