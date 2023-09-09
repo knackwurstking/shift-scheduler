e9c626fe85f891aad419ba9686c649192b07bbc4
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-09 03:19:07 +0200

    open db and adding some function +todos

--- src/lib/js/db/index.js
+++ src/lib/js/db/index.js
@@ -1,22 +1,55 @@
-export const currentVersion = 1;
-export const db = indexedDB.open("db", currentVersion);
+const currentVersion = 1;
+const request = indexedDB.open("db", currentVersion);
 
-// TODO: custom shifts and notes per day (primary key (date): "YYYY-MM-DD")
+export let db;
 
-db.onupgradeneeded = (ev) => {
+request.onupgradeneeded = (ev) => {
     switch (ev.oldVersion) {
         case 0:
-            const result = db.result;
+            const result = request.result;
             if (!result.objectStoreNames.contains("data")) {
-                result.createObjectStore("data", { keyPath: "date" });
+                const store = result.createObjectStore("data");
+                store.createIndex("shift", "note", { unique: false })
+                store.createIndex("note", "note", { unique: false })
+                store.createIndex("date", "date", { unique: true })
             }
     }
 }
 
-db.onerror = () => {
-    console.error("DB Error:", db.error);
+request.onerror = () => {
+    console.error("DB Error:", request.error);
 }
 
-db.onsuccess = () => {
-    // TODO: working with db object
+request.onsuccess = () => {
+    db = request.result;
+    console.debug("DB Ready!");
+}
+
+/**
+ * 
+ * @param {Date} date 
+ * @returns {StorageItem | null}
+ */
+export function get(date) {
+    // TODO: get shift item and note from storage or return null
+
+    return null
+}
+
+/**
+ * 
+ * @param {Date} date 
+ * @param {ShiftItem} shift 
+ * @param {Note} note 
+ */
+export function set(date, shift, note) {
+    // TODO: store (or replace) shift and note for date
+}
+
+/**
+ * 
+ * @param {Date} date 
+ */
+export function remove(date) {
+    // TODO: remove data for keyPath
 }
