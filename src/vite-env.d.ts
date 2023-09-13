b014441d64fc01607fff3ea54b26df28d20e715a
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-13 05:00:28 +0200

    remove old .calendar component and some more clean up

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,36 +3,3 @@
 
 declare type Views = "calendar" | "settings";
 declare type Themes = "default" | "custom" | "green";
-
-/*
-declare interface DayData {
-    shift: Shift | null;
-    note: string;
-}
-
-declare interface Day { // TODO: `GridItemData` replaced with Day 
-    date: Date;
-    
-    today: boolean;
-    disable: boolean;
-
-    defaultShift: Shift | null;
-    data: DayData;
-}
-
-declare interface Month {
-    data: Day[]; // NOTE: length: 42
-    monthCount: number;
-}
-
-declare interface DatePickerCurrent {
-    year: number;
-    month: number;
-}
-
-declare interface StorageItem {
-    shift: Shift | null;
-    note: string | null;
-    date: Date;
-}
-*/
