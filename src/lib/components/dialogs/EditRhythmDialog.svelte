19192010dced2647053c55132e2305d775c2c045
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-19 19:42:08 +0200

    update styles (positoning)

--- src/lib/components/dialogs/EditRhythmDialog.svelte
+++ src/lib/components/dialogs/EditRhythmDialog.svelte
@@ -1,97 +1,113 @@
 <script>
-  import { createEventDispatcher } from "svelte";
-  const dispatch = createEventDispatcher();
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
 
-  import Shift from "../shift";
+    import Shift from "../shift";
 
-  import * as settings from "../../js/settings";
+    import * as settings from "../../js/settings";
 
-  /**
-   * @type {import("../settings").Shift[]}
-   */
-  export let shifts;
-  /**
-   * @type {number[]}
-   */
-  export let rhythm;
+    /**
+     * @type {import("../settings").Shift[]}
+     */
+    export let shifts;
+    /**
+     * @type {number[]}
+     */
+    export let rhythm;
 </script>
 
 <dialog open>
-  <article>
-    <h6>Pick a shift from the bottom to add.</h6>
+    <article>
+        <div>
+            <h6>Shift Rhythm</h6>
 
-    <ul>
-      {#each rhythm as id, index}
-        {#if !!settings.getShift(id)}
-          <Shift
-            {...settings.getShift(id)}
-            on:click={() => {
-              rhythm = [...rhythm.slice(0, index), ...rhythm.slice(index + 1)];
-            }}
-          />
-        {/if}
-      {/each}
-    </ul>
+            <div class="spacer" />
 
-    <div class="shifts-container">
-      {#each shifts as shift}
-        <div class="container">
-          <Shift
-            {...shift}
-            on:click={() => {
-              rhythm = [...rhythm, shift.id];
-            }}
-          />
+            <ul>
+                {#each rhythm as id, index}
+                    {#if !!settings.getShift(id)}
+                        <Shift
+                            {...settings.getShift(id)}
+                            on:click={() => {
+                                rhythm = [...rhythm.slice(0, index), ...rhythm.slice(index + 1)];
+                            }}
+                        />
+                    {/if}
+                {/each}
+            </ul>
+
+            <div class="spacer" />
+
+            <div class="shifts-container">
+                {#each shifts as shift}
+                    <div class="container">
+                        <Shift
+                            {...shift}
+                            on:click={() => {
+                                rhythm = [...rhythm, shift.id];
+                            }}
+                        />
+                    </div>
+                {/each}
+            </div>
+
+            <div class="spacer" />
+
+            <button
+                type="submit"
+                on:click={() => dispatch("submit", rhythm)}
+            >
+                OK
+            </button>
         </div>
-      {/each}
-    </div>
-
-    <button type="submit" on:click={() => dispatch("submit", rhythm)}>OK</button
-    >
-  </article>
+    </article>
 </dialog>
 
 <style>
-  article {
-    height: 90%;
-    width: 100%;
+    .spacer {
+        border-bottom: 1px solid var(--muted-border-color);
+        margin-bottom: var(--spacing);
+    }
 
-    overflow: hidden;
-  }
+    article {
+        height: 90%;
+        width: 100%;
 
-  article > h6 {
-    height: 2em;
-  }
+        overflow: hidden;
+    }
 
-  article > ul {
-    height: calc(100% - 12rem);
+    article > div {
+        width: 100%;
+        height: 100%;
+    }
 
-    overflow: hidden;
-    overflow-y: auto;
+    article > div > ul {
+        height: calc(100% - 17em);
 
-    border-bottom: 1px solid var(--muted-border-color);
+        overflow: hidden;
+        overflow-y: auto;
 
-    padding: 0;
-  }
+        padding: 0;
+    }
 
-  article .shifts-container {
-    overflow: hidden;
-    overflow-x: auto;
+    .shifts-container {
+        height: fit-content;
 
-    -ms-overflow-style: none;
-    scrollbar-width: none;
+        overflow: hidden;
+        overflow-x: auto;
 
-    display: flex;
+        -ms-overflow-style: none;
+        scrollbar-width: none;
 
-    border-bottom: 1px solid var(--muted-border-color);
-  }
+        display: flex;
+    }
 
-  article .shifts-container::-webkit-scrollbar {
-      display: none;
-  }
+    .shifts-container::-webkit-scrollbar {
+        display: none;
+    }
 
-  article .shifts-container .container {
-    width: calc(4.5em + 16px);
-    margin: calc(var(--spacing) / 2);
-  }
+    .shifts-container .container {
+        width: calc(4.5em + 16px);
+        margin: calc(var(--spacing) / 2);
+    }
 </style>
