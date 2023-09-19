d1ca76d3c8dd66ce4269fe3043d21b26762f5d82
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-19 06:58:22 +0200

    ripple time 500 -> 250s

--- src/lib/components/shift/Shift.svelte
+++ src/lib/components/shift/Shift.svelte
@@ -1,86 +1,90 @@
 <script>
-  import { createEventDispatcher } from "svelte";
-  const dispatch = createEventDispatcher();
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
 
-  import * as ripple from "../../ripple";
+    import * as ripple from "../../ripple";
 
-  /** @type {string} */
-  export let name;
-  /** @type {string} */
-  export let shortName = "";
-  /** @type {boolean} */
-  export let visible = true;
-  /** @type {string} */
-  export let color = "var(--color)";
-  /** @type {number} */
-  export let id;
+    /** @type {string} */
+    export let name;
+    /** @type {string} */
+    export let shortName = "";
+    /** @type {boolean} */
+    export let visible = true;
+    /** @type {string} */
+    export let color = "var(--color)";
+    /** @type {number} */
+    export let id;
 
-  export let active = false;
+    export let active = false;
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div
-  class="card"
-  class:active
-  on:click={(ev) => {
-    ripple.add(ev, ev.currentTarget, { mode: "primary" });
-    dispatch("click");
-  }}
+    class="card"
+    class:active
+    on:pointerdown={(ev) => {
+        if (ev.buttons === 1) {
+            ripple.add(ev, ev.currentTarget, { mode: "primary" });
+        }
+    }}
+    on:click={(ev) => {
+        dispatch("click");
+    }}
 >
-  <div class="background" style={`background-color: ${color};`} />
-  <span class="name">{name}</span><br />
-  <span class="short-name" class:visible>{shortName || name[0]}</span>
+    <div class="background" style={`background-color: ${color};`} />
+    <span class="name">{name}</span><br />
+    <span class="short-name" class:visible>{shortName || name[0]}</span>
 </div>
 
 <style>
-  div.card {
-    position: relative;
-    overflow: hidden;
+    div.card {
+        position: relative;
+        overflow: hidden;
 
-    display: inline-block;
+        display: inline-block;
 
-    border: 1px solid var(--muted-border-color);
-    border-radius: var(--border-radius);
+        border: 1px solid var(--muted-border-color);
+        border-radius: var(--border-radius);
 
-    height: calc(3em + 16px);
-    width: calc(4.75em + 16px);
-    padding: 8px;
-    margin: 2px;
+        height: calc(3em + 16px);
+        width: calc(4.75em + 16px);
+        padding: 8px;
+        margin: 2px;
 
-    user-select: none;
-  }
+        user-select: none;
+    }
 
-  div.card.active {
-    border-color: var(--primary);
-  }
+    div.card.active {
+        border-color: var(--primary);
+    }
 
-  div.card .background {
-    position: absolute;
-    top: 0;
-    right: 0;
-    width: 100%;
-    height: 100%;
+    div.card .background {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
 
-    opacity: 0.25;
-  }
+        opacity: 0.25;
+    }
 
-  div.card span.name {
-    display: inline-block;
-    font-size: 0.95em;
-    font-weight: 400;
-    text-overflow: ellipsis;
-  }
+    div.card span.name {
+        display: inline-block;
+        font-size: 0.95em;
+        font-weight: 400;
+        text-overflow: ellipsis;
+    }
 
-  div.card span.short-name {
-    display: inline-block;
-    font-size: 0.8em;
-    font-weight: 400;
-    font-style: italic;
-    text-overflow: ellipsis;
-  }
+    div.card span.short-name {
+        display: inline-block;
+        font-size: 0.8em;
+        font-weight: 400;
+        font-style: italic;
+        text-overflow: ellipsis;
+    }
 
-  div.card span.short-name:not(.visible) {
-    display: none;
-  }
+    div.card span.short-name:not(.visible) {
+        display: none;
+    }
 </style>
