2454a339c87c5c73745d280e10a73d6381f12760
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-20 06:57:29 +0200

    adding top app bar icon-button margins

--- src/App.svelte
+++ src/App.svelte
@@ -1,304 +1,310 @@
 <script>
-  import "./app.css";
+    import "./app.css";
 
-  import { onMount } from "svelte";
-  import { App } from "@capacitor/app";
+    import { onMount } from "svelte";
+    import { App } from "@capacitor/app";
 
-  import TiArrowBackOutline from "svelte-icons/ti/TiArrowBackOutline.svelte";
-  import TiSpanner from "svelte-icons/ti/TiSpanner.svelte";
-  import MdModeEdit from "svelte-icons/md/MdModeEdit.svelte";
-  import MdToday from "svelte-icons/md/MdToday.svelte";
+    import TiArrowBackOutline from "svelte-icons/ti/TiArrowBackOutline.svelte";
+    import TiSpanner from "svelte-icons/ti/TiSpanner.svelte";
+    import MdModeEdit from "svelte-icons/md/MdModeEdit.svelte";
+    import MdToday from "svelte-icons/md/MdToday.svelte";
 
-  //import * as ripple from "./lib/js/ripple";
-  import * as utils from "./lib/js/utils";
-  import * as db from "./lib/js/db";
-  import * as settings from "./lib/js/settings";
+    //import * as ripple from "./lib/js/ripple";
+    import * as utils from "./lib/js/utils";
+    import * as db from "./lib/js/db";
+    import * as settings from "./lib/js/settings";
 
-  import { EditDayDialog } from "./lib/components/dialogs";
-  import DatePicker from "./lib/components/date-picker";
-  import IconButton from "./lib/components/icon-button";
-  import Calendar from "./lib/components/calendar";
-  import SettingsView from "./lib/components/settings";
-  import ShiftCard from "./lib/components/shift";
+    import { EditDayDialog } from "./lib/components/dialogs";
+    import DatePicker from "./lib/components/date-picker";
+    import IconButton from "./lib/components/icon-button";
+    import Calendar from "./lib/components/calendar";
+    import SettingsView from "./lib/components/settings";
+    import ShiftCard from "./lib/components/shift";
 
-  /** @type {Themes} */
-  let currentTheme = "custom";
-  /** @type {Views} */
-  let view = "calendar";
-  /** @type {Views[]}*/
-  let viewStack = [view];
-  /** @type {Date} */
-  let currentDate = new Date();
-  /** @type {boolean} */
-  let editMode_open = false;
-  /** @type {number} - -2 will remove the custom shift from the database */
-  let editMode_index = -1;
+    /** @type {Themes} */
+    let currentTheme = "custom";
+    /** @type {Views} */
+    let view = "calendar";
+    /** @type {Views[]}*/
+    let viewStack = [view];
+    /** @type {Date} */
+    let currentDate = new Date();
+    /** @type {boolean} */
+    let editMode_open = false;
+    /** @type {number} - -2 will remove the custom shift from the database */
+    let editMode_index = -1;
 
-  /** @type {Calendar} */
-  let calendar;
+    /** @type {Calendar} */
+    let calendar;
 
-  /** @type {boolean} */
-  let editDayDialog_open = false;
-  /** @type {Date} */
-  let editDayDialog_date;
+    /** @type {boolean} */
+    let editDayDialog_open = false;
+    /** @type {Date} */
+    let editDayDialog_date;
 
-  /** @type {Date} */
-  let today = new Date();
+    /** @type {Date} */
+    let today = new Date();
 
-  $: {
-    if (view === "calendar") {
-      settings.load();
-      currentTheme = settings.data.currentTheme;
-      if (settings.data.mode !== "auto") {
-        document.documentElement.setAttribute("data-theme", settings.data.mode);
-      } else {
-        document.documentElement.removeAttribute("data-theme");
-      }
+    $: {
+        if (view === "calendar") {
+            settings.load();
+            currentTheme = settings.data.currentTheme;
+            if (settings.data.mode !== "auto") {
+                document.documentElement.setAttribute("data-theme", settings.data.mode);
+            } else {
+                document.documentElement.removeAttribute("data-theme");
+            }
+        }
     }
-  }
 
-  $: calendar && initCalendar();
+    $: calendar && initCalendar();
 
-  function initCalendar() {
-    calendar.set(currentDate);
-  }
-
-  function goBackInHistory() {
-    if (viewStack.length === 1) return;
-    viewStack.pop();
-    viewStack = viewStack;
-    view = viewStack[viewStack.length - 1];
-  }
-
-  /**
-   *
-   * @param {Views} v
-   */
-  function goTo(v) {
-    editMode_open = false;
-    viewStack = [...viewStack, v];
-    view = v;
-  }
-
-  /**
-   *
-   * @param {Date | null} date
-   */
-  function setCurrentDate(date) {
-    if (date) currentDate = date;
-    calendar.set(currentDate);
-  }
-
-  /**
-   * @returns {import("./lib/js/settings").Shift | "reset"}
-   */
-  function getEditModeShift() {
-    if (editMode_index === -2)
-      return "reset";
-    return settings.data.shifts[editMode_index] || null;
-  }
-
-  // TODO: need to update today after 12:00 AM
-
-  onMount(async () => {
-    if (utils.isAndroid()) {
-      App.addListener("backButton", () => goBackInHistory());
+    function initCalendar() {
+        calendar.set(currentDate);
     }
-  });
+
+    function goBackInHistory() {
+        if (viewStack.length === 1) return;
+        viewStack.pop();
+        viewStack = viewStack;
+        view = viewStack[viewStack.length - 1];
+    }
+
+    /**
+     *
+     * @param {Views} v
+     */
+    function goTo(v) {
+        editMode_open = false;
+        viewStack = [...viewStack, v];
+        view = v;
+    }
+
+    /**
+     *
+     * @param {Date | null} date
+     */
+    function setCurrentDate(date) {
+        if (date) currentDate = date;
+        calendar.set(currentDate);
+    }
+
+    /**
+     * @returns {import("./lib/js/settings").Shift | "reset"}
+     */
+    function getEditModeShift() {
+        if (editMode_index === -2) return "reset";
+        return settings.data.shifts[editMode_index] || null;
+    }
+
+    // TODO: need to update today after 12:00 AM
+
+    onMount(async () => {
+        if (utils.isAndroid()) {
+            App.addListener("backButton", () => goBackInHistory());
+        }
+    });
 </script>
 
 <svelte:head>
-  <link rel="stylesheet" href="/css/themes/{currentTheme}.min.css" />
+    <link rel="stylesheet" href="/css/themes/{currentTheme}.min.css" />
 </svelte:head>
 
 {#if editDayDialog_open}
-  <EditDayDialog
-    date={editDayDialog_date}
-    on:submit={async ({ detail }) => {
-      editDayDialog_open = false;
-      db.setDataForDay(editDayDialog_date, detail.shift, detail.note);
-      calendar.reload();
-    }}
-  />
+    <EditDayDialog
+        date={editDayDialog_date}
+        on:submit={async ({ detail }) => {
+            editDayDialog_open = false;
+            db.setDataForDay(editDayDialog_date, detail.shift, detail.note);
+            calendar.reload();
+        }}
+    />
 {/if}
 
 <header class="container-fluid" style="font-size: 1rem;">
-  <!-- Top app bar -->
+    <!-- Top app bar -->
 
-  {#if view !== viewStack[0]}
-    <div class="actions">
-      <IconButton on:click={() => goBackInHistory()}>
-        <TiArrowBackOutline />
-      </IconButton>
-    </div>
-  {/if}
-
-  {#if view === "calendar"}
-    <!--DatePicker style="margin: 0; width: 30rem;" bind:monthCount={currentMonthCount} /-->
-    <DatePicker
-      style="margin: 0; width: 30rem;"
-      bind:currentDate
-      on:currentdatechanged={() => setCurrentDate(null)}
-    />
-  {:else if view === "settings"}
-    <h1 style="margin: 0; margin-left: 8px;">Settings</h1>
-  {/if}
-
-  <span class="spacer" />
-
-  <div class="actions">
-    {#if view === "calendar"}
-      <!-- Edit Shifts -->
-      <IconButton
-        on:click={() => {
-          editMode_open = !editMode_open;
-        }}><MdModeEdit /></IconButton
-      >
-
-      <!-- GoTo Today -->
-      <IconButton
-        disabled={today.getFullYear() === currentDate.getFullYear() &&
-          today.getMonth() === currentDate.getMonth()}
-        on:click={() => setCurrentDate(new Date())}
-      >
-        <MdToday />
-      </IconButton>
-
-      <!-- Settings -->
-      <IconButton on:click={() => goTo("settings")}><TiSpanner /></IconButton>
+    {#if view !== viewStack[0]}
+        <div class="actions">
+            <IconButton on:click={() => goBackInHistory()}>
+                <TiArrowBackOutline />
+            </IconButton>
+        </div>
     {/if}
-  </div>
+
+    {#if view === "calendar"}
+        <!--DatePicker style="margin: 0; width: 30rem;" bind:monthCount={currentMonthCount} /-->
+        <DatePicker
+            style="margin: 0; width: 30rem;"
+            bind:currentDate
+            on:currentdatechanged={() => setCurrentDate(null)}
+        />
+    {:else if view === "settings"}
+        <h1 style="margin: 0; margin-left: 8px;">Settings</h1>
+    {/if}
+
+    <span class="spacer" />
+
+    <div class="actions">
+        {#if view === "calendar"}
+            <!-- Edit Shifts -->
+            <IconButton
+                margin="8px 4px"
+                on:click={() => {
+                    editMode_open = !editMode_open;
+                }}><MdModeEdit /></IconButton
+            >
+
+            <!-- GoTo Today -->
+            <IconButton
+                margin="8px 4px"
+                disabled={today.getFullYear() === currentDate.getFullYear() &&
+                    today.getMonth() === currentDate.getMonth()}
+                on:click={() => setCurrentDate(new Date())}
+            >
+                <MdToday />
+            </IconButton>
+
+            <!-- Settings -->
+            <IconButton margin="8px 4px" on:click={() => goTo("settings")}>
+                <TiSpanner />
+            </IconButton>
+        {/if}
+    </div>
 </header>
 
-<main
-  class="container-fluid"
-  style={`bottom: ${editMode_open ? "calc(3em + 22px)" : "1px"}`}
->
-  {#if view === "calendar"}
-    <Calendar
-      bind:this={calendar}
-      on:click={async ({ detail }) => {
-        if (detail && editMode_open) {
-          let shift = getEditModeShift();
-          if (!shift) {
-            return;
-          }
+<main class="container-fluid" style={`bottom: ${editMode_open ? "calc(3em + 22px)" : "1px"}`}>
+    {#if view === "calendar"}
+        <Calendar
+            bind:this={calendar}
+            on:click={async ({ detail }) => {
+                if (detail && editMode_open) {
+                    let shift = getEditModeShift();
+                    if (!shift) {
+                        return;
+                    }
 
-          const { note } = db.getDataForDay(detail);
+                    const { note } = db.getDataForDay(detail);
 
-          if (shift === "reset") {
-            shift = null;
-            if (note) {
-              db.setDataForDay(detail, null, note);
-            } else {
-              db.removeDataForDay(detail);
-            }
-          } else {
-            db.setDataForDay(detail, shift, note);
-          }
+                    if (shift === "reset") {
+                        shift = null;
+                        if (note) {
+                            db.setDataForDay(detail, null, note);
+                        } else {
+                            db.removeDataForDay(detail);
+                        }
+                    } else {
+                        db.setDataForDay(detail, shift, note);
+                    }
 
-          calendar.reload();
-        } else if (detail) {
-          editDayDialog_date = detail;
-          editDayDialog_open = true;
-        }
-      }}
-      on:currentdatechange={({ detail }) => (currentDate = detail)}
-    />
-  {:else if view === "settings"}
-    <SettingsView />
-  {/if}
+                    calendar.reload();
+                } else if (detail) {
+                    editDayDialog_date = detail;
+                    editDayDialog_open = true;
+                }
+            }}
+            on:currentdatechange={({ detail }) => (currentDate = detail)}
+        />
+    {:else if view === "settings"}
+        <SettingsView />
+    {/if}
 </main>
 
 <footer class="container-fluid" class:visible={editMode_open}>
-  {#if editMode_open && !!settings}
-    <span>
-      <ShiftCard
-        name="Reset"
-        visible={false}
-        active={editMode_index === -2}
-        color="var(--color)"
-        id={-2}
-        on:click={() => {
-          editMode_index = editMode_index === -2 ? -1 : -2;
-        }}
-      />
-    </span>
-
-    {#key settings.data}
-      {#each settings.data.shifts as shift, index}
+    {#if editMode_open && !!settings}
         <span>
-          <ShiftCard
-            {...shift}
-            active={editMode_index == index}
-            on:click={() => {
-              editMode_index = editMode_index === index ? -1 : index;
-            }}
-          />
+            <ShiftCard
+                name="Reset"
+                visible={false}
+                active={editMode_index === -2}
+                color="var(--color)"
+                id={-2}
+                on:click={() => {
+                    editMode_index = editMode_index === -2 ? -1 : -2;
+                }}
+            />
         </span>
-      {/each}
-    {/key}
-  {/if}
+
+        {#key settings.data}
+            {#each settings.data.shifts as shift, index}
+                <span>
+                    <ShiftCard
+                        {...shift}
+                        active={editMode_index == index}
+                        on:click={() => {
+                            editMode_index = editMode_index === index ? -1 : index;
+                        }}
+                    />
+                </span>
+            {/each}
+        {/key}
+    {/if}
 </footer>
 
 <style>
-  :global(html, body) {
-    overflow: hidden;
-  }
+    :global(html, body) {
+        overflow: hidden;
+    }
 
-  :global(button) {
-    position: relative;
-    overflow: hidden;
-  }
+    :global(button) {
+        position: relative;
+        overflow: hidden;
+    }
 
-  :global(*) {
-    box-sizing: border-box;
-  }
+    :global(*) {
+        box-sizing: border-box;
+    }
 
-  header {
-    position: relative;
-    height: 60px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    border-bottom: 1px solid var(--muted-border-color);
-  }
+    header {
+        position: relative;
+        height: 60px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        border-bottom: 1px solid var(--muted-border-color);
+    }
 
-  header > .spacer {
-    width: 100%;
-    border: none;
-  }
+    header > .spacer {
+        width: 100%;
+        border: none;
+    }
 
-  header > .actions {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    height: 100%;
-  }
+    header > .actions {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        height: 100%;
+    }
 
-  main {
-    position: fixed;
-    top: 60px;
-    right: 0;
-    left: 0;
-    padding: 0;
-  }
+    main {
+        position: fixed;
+        top: 60px;
+        right: 0;
+        left: 0;
+        padding: 0;
+    }
 
-  footer {
-    transform: none;
-    height: calc(3em + 22px);
-    right: 0;
-    bottom: 0;
-    left: 0;
-    position: absolute;
-    border-top: 1px solid var(--muted-border-color);
-    display: flex;
-    overflow: hidden;
-    overflow-x: auto;
-    background-color: var(--background-color);
-  }
+    footer {
+        transform: none;
+        height: calc(3em + 22px);
+        right: 0;
+        bottom: 0;
+        left: 0;
+        position: absolute;
+        border-top: 1px solid var(--muted-border-color);
+        display: flex;
+        overflow: hidden;
+        overflow-x: auto;
+        background-color: var(--background-color);
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+    }
 
-  footer:not(.visible) {
-    transform: translateY(100%);
-  }
+    footer::-webkit-scrollbar {
+        display: none;
+    }
+
+    footer:not(.visible) {
+        transform: translateY(100%);
+    }
 </style>
