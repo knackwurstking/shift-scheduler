2d137e3804bf101f8516d1359e4b355fb847de4d
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-25 03:34:33 +0200

    sort storage keys && formatting...

--- src/lib/components/settings/View.svelte
+++ src/lib/components/settings/View.svelte
@@ -1,6 +1,8 @@
 <script>
     import IoIosTrash from "svelte-icons/io/IoIosTrash.svelte";
     import IoIosOpen from "svelte-icons/io/IoIosOpen.svelte";
+    import IoMdCloudDownload from "svelte-icons/io/IoMdCloudDownload.svelte";
+    import IoMdCloudUpload from "svelte-icons/io/IoMdCloudUpload.svelte";
 
     import Shift, { ShiftAdd } from "../shift";
     import { EditRhythmDialog, AddShiftDialog, EditShiftDialog, StorageDialog } from "../dialogs";
@@ -9,6 +11,7 @@
     import * as settings from "../../js/settings";
     import * as db from "../../js/db";
     import { ripple } from "../../js/ripple";
+    let _ripple = ripple({ usePointer: true });
     let _primaryRipple = ripple({ color: "var(--ripple-primary-color)", usePointer: true });
     let _secondaryRipple = ripple({ color: "var(--ripple-secondary-color)", usePointer: true });
 
@@ -209,18 +212,12 @@
         <h2>Data Storage</h2>
 
         <div class="button-group">
-            <button
-                class="upload outline"
-                use:_primaryRipple
-            >
-                Upload
+            <button class="upload secondary outline" use:_ripple>
+                <IoMdCloudUpload />
             </button>
 
-            <button
-                class="download outline"
-                use:_primaryRipple
-            >
-                Download
+            <button class="download secondary outline" use:_ripple>
+                <IoMdCloudDownload />
             </button>
         </div>
 
@@ -236,48 +233,48 @@
                     </thead>
                     <tbody>
                         {#key reloadDataStorageTable}
-                            {#each db.list() as key}
+                            {#each db
+                                .list()
+                                .sort((a, b) => (a.getFullYear() > b.getFullYear() ? 1 : -1))
+                                .sort( (a, b) => (a.getMonth() > b.getMonth() && a.getFullYear() === b.getFullYear() ? 1 : -1) ) as key}
                                 <tr>
                                     <td>{key.getFullYear()}</td>
                                     <td>{key.getMonth() + 1}</td>
                                     <td class="actions">
-                                            <IconButton
-                                                style={`
+                                        <IconButton
+                                            style={`
                                                     margin: 4px;
                                                 `}
-                                                on:click={() => {
-                                                    storageDialog_year = key.getFullYear();
-                                                    storageDialog_month = key.getMonth();
-                                                    storageDialog_open = true;
-                                                }}
-                                            >
-                                                <IoIosOpen />
-                                            </IconButton>
+                                            on:click={() => {
+                                                storageDialog_year = key.getFullYear();
+                                                storageDialog_month = key.getMonth();
+                                                storageDialog_open = true;
+                                            }}
+                                        >
+                                            <IoIosOpen />
+                                        </IconButton>
 
-                                            <IconButton
-                                                style={`
+                                        <IconButton
+                                            style={`
                                                     margin: 4px;
                                                 `}
-                                                on:click={() => {
-                                                    const yes = window.confirm(
-                                                        `Delete all data for "${key.getFullYear()}/${(
-                                                            key.getMonth() + 1
-                                                        )
-                                                            .toString()
-                                                            .padStart(2, "0")}" ?`
-                                                    );
-                                                    if (yes) {
-                                                        db.remove(
-                                                            key.getFullYear(),
-                                                            key.getMonth()
-                                                        );
-                                                        reloadDataStorageTable =
-                                                            !reloadDataStorageTable;
-                                                    }
-                                                }}
-                                            >
-                                                <IoIosTrash />
-                                            </IconButton>
+                                            on:click={() => {
+                                                const yes = window.confirm(
+                                                    `Delete all data for "${key.getFullYear()}/${(
+                                                        key.getMonth() + 1
+                                                    )
+                                                        .toString()
+                                                        .padStart(2, "0")}" ?`
+                                                );
+                                                if (yes) {
+                                                    db.remove(key.getFullYear(), key.getMonth());
+                                                    reloadDataStorageTable =
+                                                        !reloadDataStorageTable;
+                                                }
+                                            }}
+                                        >
+                                            <IoIosTrash />
+                                        </IconButton>
                                     </td>
                                 </tr>
                             {/each}
@@ -344,12 +341,29 @@
         user-select: none;
     }
 
+    article.data-storage {
+        position: relative;
+    }
+
+    article.data-storage > h2 {
+        max-width: calc(100% - 120px - 1rem);
+    }
+
     article.data-storage .button-group {
+        position: absolute;
+        top: 1rem;
+        right: 1rem;
+        margin-top: var(--block-spacing-vertical);
+
         display: flex;
     }
 
     article.data-storage .button-group button {
         border-radius: 0;
+        width: 60px;
+        height: 40px;
+        padding: 4px;
+        box-shadow: var(--button-box-shadow);
     }
 
     article.data-storage .button-group button:first-child {
