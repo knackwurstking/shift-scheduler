7ea18536fb50d87e30445efe3592aaa5695b8bd8
Udo Bauer <knackwurstking.tux@gmail.com>
2023-09-28 21:12:48 +0200

    adding update function and update typedefs

--- src/lib/js/ui/dialog/date-picker.js
+++ src/lib/js/ui/dialog/date-picker.js
@@ -1,19 +1,32 @@
 import * as utils from "../../utils";
 
 /**
+ * @typedef DatePickerDialogProps
+ * @type {{
+ *  onsubmit: (ev: CustomEvent<{ year: number, month: number }>) => (void|Promise<void>);
+ *  oncancel: (ev: CustomEvent<void>) => (void|Promise<void>);
+ * }}
+ */
+
+/**
  * 
  * @param {number} year 
  * @param {number} month 
- * @param {{
- *  onsubmit: (ev: CustomEvent<{ year: number, month: number }>) => (void|Promise<void>);
- *  oncancel: (ev: CustomEvent<void>) => (void|Promise<void>);
- * }} props 
+ * @param {DatePickerDialogProps} props 
+ * @returns {HTMLDialogElement}
  */
 export async function create(year, month, props) {
     const dialog = document.createElement("dialog");
 
     dialog.classList.add("date-picker--dialog");
     dialog.open = false;
+
+    if (props?.oncancel) {
+        dialog.addEventListener("cancel", props.oncancel);
+    }
+    if (props?.onsubmit) {
+        dialog.addEventListener("submit", props.onsubmit);
+    }
 
     // TODO: add children ...
     if (utils.isAndroid()) {
@@ -26,5 +39,22 @@ export async function create(year, month, props) {
             <article>
             </article>
         `;
+    }
+
+    return dialog;
+}
+
+/**
+ * 
+ * @param {HTMLDialogElement} dialog
+ * @param {number} year 
+ * @param {number} month 
+ */
+export async function update(dialog, year, month) {
+    // TODO: update input year and month
+    if (utils.isAndroid()) {
+        // ...
+    } else {
+        // ...
     }
 }
