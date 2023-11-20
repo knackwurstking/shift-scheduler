e041883f6442690df6bd9d5b3e1b06b1b82169b4
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-20 17:11:41 +0100

    adding container div for data-theme=light preview and remove todo

--- src/view/pdf/View.svelte
+++ src/view/pdf/View.svelte
@@ -1,5 +1,4 @@
 <script>
-    // TODO: generate data table for the current year and convert html into pdf
     import { jsPDF } from "jspdf";
 
     import { Button } from "svelte-css";
@@ -16,22 +15,24 @@
     <!-- TODO: pick a year? -->
     <!-- TODO: display table for each month -->
 
-    <table id="pdf-content" data-theme="light">
-        <thead>
-            <tr>
-                <th class="is-text-left">Left Header</th>
-                <th class="is-text-center">Center Header</th>
-                <th class="is-text-right">Right Header</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td class="is-text-left">left body</td>
-                <td class="is-text-center">center body</td>
-                <td class="is-text-right">right body</td>
-            </tr>
-        </tbody>
-    </table>
+    <div id="pdf-content" class="has-padding" data-theme="light">
+        <table>
+            <thead>
+                <tr>
+                    <th class="is-text-left">Left Header</th>
+                    <th class="is-text-center">Center Header</th>
+                    <th class="is-text-right">Right Header</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="is-text-left">left body</td>
+                    <td class="is-text-center">center body</td>
+                    <td class="is-text-right">right body</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 
     <section>
         <Button.Root
