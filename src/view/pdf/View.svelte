324d1aa5371e220afff48619794dcf1db2c65894
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-20 16:45:56 +0100

    playing around with jspdf the first time

--- src/view/pdf/View.svelte
+++ src/view/pdf/View.svelte
@@ -1,12 +1,53 @@
 <script>
     // TODO: generate data table for the current year and convert html into pdf
+    import { jsPDF } from "jspdf";
+
+    import { Button } from "svelte-css";
+
+    const pdf = new jsPDF("portrait", "mm", "a4");
 </script>
 
 <div
     class="ui-container is-max  no-scrollbar"
     style:padding-top="3em"
+    style:overflow="auto"
 >
     <!-- TODO: adding a print/share button on top? -->
     <!-- TODO: pick a year? -->
     <!-- TODO: display table for each month -->
+
+    <table id="pdf-content" data-theme="light">
+        <thead>
+            <tr>
+                <th class="is-text-left">Left Header</th>
+                <th class="is-text-center">Center Header</th>
+                <th class="is-text-right">Right Header</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="is-text-left">left body</td>
+                <td class="is-text-center">center body</td>
+                <td class="is-text-right">right body</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <section>
+        <Button.Root
+            on:click={() => {
+                const el = document.getElementById("pdf-content");
+                pdf.html(el, {
+                    callback: (doc) => {
+                        doc.save();
+                    },
+                    margin: [10, 10, 10, 10],
+                    width: 190,
+                    windowWidth: el.clientWidth, //window width in CSS pixels
+                });
+            }}
+        >
+            Print
+        </Button.Root>
+    </section>
 </div>
