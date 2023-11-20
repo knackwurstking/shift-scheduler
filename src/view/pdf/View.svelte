41dea585cd657e9a68848ddc3ecffc001edec175
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-20 14:33:43 +0100

    pdf view: adding root container and some todos

--- src/view/pdf/View.svelte
+++ src/view/pdf/View.svelte
@@ -1,3 +1,12 @@
 <script>
     // TODO: generate data table for the current year and convert html into pdf
 </script>
+
+<div
+    class="ui-container is-max  no-scrollbar"
+    style:padding-top="3em"
+>
+    <!-- TODO: adding a print/share button on top? -->
+    <!-- TODO: pick a year? -->
+    <!-- TODO: display table for each month -->
+</div>
