efa2837a64525d388b54b4c6c95dfb4eb3558ac2
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-21 16:01:03 +0100

    using utils getDaysForMonth and remove getDays()

--- src/view/calendar/Content.svelte
+++ src/view/calendar/Content.svelte
@@ -5,9 +5,10 @@
 
     import Day from "./Day.svelte";
 
-    import * as db from "../../lib/js/db";
-
     import * as Store from "../../lib/stores";
+
+    import * as db from "../../lib/js/db";
+    import * as utils from "../../lib/js/utils";
 
     /******************************
      * Variable Export Definitions
@@ -15,16 +16,19 @@
 
     /** @type {number} */
     export let index;
+
     /** @type {Date} */
     export let currentDate;
-    $: currentDate && setDays();
+
+    $: currentDate && utils.getDaysForMonth(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        {
+            weekStart: $weekStart
+        }
+    ).then(result => days = result);
+
     $: index === 1 && currentDate && dispatch("currentdatechange", currentDate);
-
-    /********************
-     * Store: shift-setup
-     ********************/
-
-    const shiftSetup = Store.shiftSetup.create();
 
     /********************
      * Store: week-start
@@ -40,43 +44,13 @@
 
     /** @type {import(".").Day[]} */
     let days = [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0,
     ].map(() => ({ date: currentDate, data: { shift: null, note: "" } }));
-
-    /***********************
-     * Function Definitions
-     ***********************/
-
-    async function setDays() {
-        const year = currentDate.getFullYear();
-        const month = currentDate.getMonth();
-
-        let startDay = new Date(year, month, 1).getDay();
-        if ($weekStart === "mon") {
-            startDay = startDay === 0 ? 6 : startDay - 1;
-        }
-
-        const data = await db.get(year, month);
-
-        for (let i = 0; i < days.length; i++) {
-            days[i].date = new Date(year, month, i + 1 - startDay);
-
-            const key = `${days[i].date.getFullYear()}-${days[
-                i
-            ].date.getMonth()}-${days[i].date.getDate()}`;
-
-            let shift = data[key]?.shift;
-            if (shift) {
-                shift = shiftSetup.getShiftForID(shift.id) || shift;
-            }
-
-            days[i].data = {
-                shift: shift || shiftSetup.calcShiftStep(days[i].date),
-                note: data[key]?.note || "",
-            };
-        }
-    }
 </script>
 
 <FlexGrid.Row class="is-max-height" gap=".1em">
