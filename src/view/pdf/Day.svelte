a735fed3690a66343bd16a4c838d2c73cf08ea74
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-22 18:11:55 +0100

    adding pdf view table header and body

--- src/view/pdf/Day.svelte
+++ src/view/pdf/Day.svelte
@@ -3,6 +3,31 @@
     export let date;
     /** @type {import("../../lib/stores/shift-setup").Shift} */
     export let shift;
-
-    // TODO: display date and shift.shortName (with color)
 </script>
+
+<td
+    class="is-text-center"
+    style:height="3em"
+    style:width="calc(100% / 7)"
+>
+    <div class="is-max-width flex column">
+        <span
+            style:display="block"
+            style:font-size=".85em"
+            style:font-weight="lighter"
+        >{date}</span>
+        <span
+            style:width="100%"
+            style:display="block"
+            style:color={shift.color || "transparent"}
+            style:text-shadow=".1em .1em .1em hsl(var(--border))"
+            style:overflow="hidden"
+            style:text-overflow="ellipsis"
+            style:white-space="nowrap"
+            style:font-size=".9em"
+        >{shift.shortName}</span>
+    </div>
+</td>
+
+<style>
+</style>
--- src/view/pdf/WeekDaysHeader.svelte
+++ src/view/pdf/WeekDaysHeader.svelte
@@ -1,8 +1,61 @@
 <script>
+    import { onDestroy } from "svelte";
+
+    import * as Store from "../../lib/stores";
+
+    import * as lang from "../../lib/js/lang";
+
+    /***********************
+     * Variable Definitions
+     ***********************/
+
+    let cleanUp = []
+
+    /** @type {string[]} */
+    let headerItems = [
+        lang.get("misc", "sun"),
+        lang.get("misc", "mon"),
+        lang.get("misc", "tue"),
+        lang.get("misc", "wed"),
+        lang.get("misc", "thu"),
+        lang.get("misc", "fri"),
+        lang.get("misc", "sat"),
+    ];
+
+    /********************
+     * Store: week-start
+     ********************/
+
+    const weekStart = Store.weekStart.create();
+    $: !!weekStart && cleanUp.push(weekStart.subscribe(weekStart => {
+        const items = [
+            lang.get("misc", "sun"),
+            lang.get("misc", "mon"),
+            lang.get("misc", "tue"),
+            lang.get("misc", "wed"),
+            lang.get("misc", "thu"),
+            lang.get("misc", "fri"),
+            lang.get("misc", "sat"),
+        ];
+
+        if (weekStart === "mon") {
+            items.push(items.shift());
+        }
+
+        headerItems = items;
+    }));
+
+    /********************
+     * Mount and Destroy
+     ********************/
+
+    onDestroy(() => cleanUp.forEach(fn => fn()));
 </script>
 
 <thead>
     <tr>
-        <!-- TODO: building header (en/de) -->
+        {#each headerItems as item}
+            <th>{item}</th>
+        {/each}
     </tr>
 </thead>
