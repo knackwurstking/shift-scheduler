e0ec836a225db860b6422ded7bce1b04726d415d
Udo Bauer <knackwurstking.tux@gmail.com>
2023-11-23 17:26:37 +0100

    update table cell sizing

--- src/view/pdf/Page.svelte
+++ src/view/pdf/Page.svelte
@@ -10,29 +10,21 @@
     export let data;
 </script>
 
-<div
-    class="has-border"
->
+<div>
     <h3>{lang.get("months", month.toString())}</h3>
 
-    <table
-        style:width="100%"
-        style:height="50%"
-    >
+    <table>
         <WeekDaysHeader />
 
         <tbody>
             {#each [[0,7],[7,14],[14,21],[21,28],[28,35],[35,42]] as i}
                 <tr>
                     {#each data.slice(i[0], i[1]) as day}
-                        {#if day.date.getMonth() === month}
-                            <Day
-                                date={day.date.getDate()}
-                                shift={day.data.shift}
-                            />
-                        {:else}
-                            <td />
-                        {/if}
+                        <Day
+                            date={day.date.getDate()}
+                            shift={day.data.shift}
+                            empty={day.date.getMonth() !== month}
+                        />
                     {/each}
                 </tr>
             {/each}
@@ -44,5 +36,12 @@
     div {
         width: 100%;
         aspect-ratio: 1 / 1.414;
+    }
+
+    table {
+        width: 100%;
+        height: fit-content;
+        max-height: 50%;
+        table-layout: fixed;
     }
 </style>
