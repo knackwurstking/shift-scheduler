e3dc00b9ec3fae0a683a715493d53ad472bed26e
Udo Bauer <knackwurstking.tux@gmail.com>
2023-12-07 08:32:56 +0100

    basic html and js for backup import/export +todo

--- src/view/settings/Backup.svelte
+++ src/view/settings/Backup.svelte
@@ -1,2 +1,46 @@
 <script>
+    import FileImportOutline from "svelte-material-icons/FileImportOutline.svelte";
+    import FileExportOutline from "svelte-material-icons/FileExportOutline.svelte";
+
+    import { Text, Button } from "svelte-css";
+
+    import * as lang from "../../lib/js/lang";
+
+    /***********************
+     * Function Definitions
+     ***********************/
+
+    async function importBackup() {
+        // TODO: import settings and storage data "shift-scheduler-backup.json"
+    }
+
+    async function exportBackup() {
+        // TODO: export settings and storage data "shift-scheduler-backup.json"
+    }
 </script>
+
+<article class="ui-card has-margin">
+    <section>
+        <Text.Label
+            primary={lang.get("settingsView", "labelupdownPrimaryText")}
+            secondary={lang.get(
+                "settingsView",
+                "labelupdownSecondaryText"
+            )}
+            row
+        >
+            <Button.Icon
+                style="margin: calc(var(--spacing) / 2)"
+                on:click={() => importBackup()}
+            >
+                <FileImportOutline />
+            </Button.Icon>
+            <Button.Icon
+                style="margin: calc(var(--spacing) / 2)"
+                on:click={() => exportBackup()}
+            >
+                <FileExportOutline />
+            </Button.Icon>
+        </Text.Label>
+</article>
+    </section>
