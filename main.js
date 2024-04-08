423d6f08c6f0d2b225deaf079c623e4f1fdd7503
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-08 21:12:31 +0200

    Add app bar component and center slot title

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./node_modules/ui/css/main.css"
 import "./style.css"
 
+import AppBar from "./lib/app-bar"
 import StackLayout from "./lib/stack-layout"
 import Storage from "./lib/storage"
 
@@ -10,15 +11,32 @@ document.querySelector('#app').innerHTML = `
     <main class="container ui-container is-debug">
         <div class="stack-layout is-max"></div>
     </main>
+
+    <header class="ui-app-bar ui-app-bar-top">
+        <div class="ui-app-bar-main">
+            <div></div>
+
+            <div>
+                <h4 class="app-bar-title"></h4>
+            </div>
+
+            <div></div>
+        </div>
+    </header>
 `
 
 async function main() {
     const themeHandler = createThemeHandler()
+
     const stackLayout = new StackLayout(
         document.querySelector("main.container > .stack-layout")
     )
 
-    // TODO: add the top-app-bar and the first page (calendar)
+    const appBar = new AppBar(
+        document.querySelector(".ui-app-bar")
+    )
+
+    // TODO: ...
 }
 
 window.addEventListener("DOMContentLoaded", main)
