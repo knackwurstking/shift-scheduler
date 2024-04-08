179bcd5359669d28e1f6bd894cb7bdd74eca1182
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-08 20:44:33 +0200

    Add the stack layout and move theme stuff to main.js

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
 import "./node_modules/ui/css/main.css"
 import "./style.css"
 
-import themeHandler from "./lib/theme-handler"
+import StackLayout from "./lib/stack-layout"
+import Storage from "./lib/storage"
+
+const storage = new Storage()
 
 document.querySelector('#app').innerHTML = `
+    <main class="container ui-container is-debug">
+        <div class="stack-layout is-max"></div>
+    </main>
 `
+
+async function main() {
+    const themeHandler = createThemeHandler()
+    const stackLayout = new StackLayout(
+        document.querySelector("main.container > .stack-layout")
+    )
+
+    // TODO: add the top-app-bar and the first page (calendar)
+}
 
 window.addEventListener("DOMContentLoaded", main)
 
-async function main() {
-    themeHandler.start()
+async function createThemeHandler() {
+    const themeHandler = new utils.theme.ThemeHandler()
+
+    themeHandler.addTheme("zinc", "/themes/zinc.css")
+    themeHandler.loadTheme(constants.theme.name)
+
+    const theme = storage.get("theme", null)
+    if (theme?.mode) {
+        themeHandler.setMode(theme.mode)
+    }
+
+    return themeHandler
 }
