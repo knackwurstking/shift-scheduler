3b604e0dd664e96b9fc9817fe39592baa3fa6e29
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-09 08:27:32 +0200

    Add all the missing app-bar icons

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-import "./node_modules/ui/css/main.css"
-import "./style.css"
+import "./node_modules/ui/css/main.css";
+import "./style.css";
 
-import { utils, ripple, svg } from "ui"
-import constants from "./lib/constants"
-import AppBar from "./lib/app-bar"
-import StackLayout from "./lib/stack-layout"
-import Storage from "./lib/storage"
-import DatePicker from "./lib/date-picker"
+import { utils, ripple, svg } from "ui";
+import constants from "./lib/constants";
+import AppBar from "./lib/app-bar";
+import StackLayout from "./lib/stack-layout";
+import Storage from "./lib/storage";
+import DatePicker from "./lib/date-picker";
 
-const storage = new Storage()
+const storage = new Storage();
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
     <main class="container ui-container is-debug">
         <div class="stack-layout is-max"></div>
     </main>
@@ -41,79 +41,79 @@ document.querySelector('#app').innerHTML = `
             </div>
 
             <div>
-                <!-- TODO: Add icons and update app-bar -->
-                    <button
-                        class="app-bar-edit-mode ui-icon-button ghost primary"
-                        style="display: none;"
-                        data-ripple="{}"
-                    >
-                    </button>
+                <button
+                    class="app-bar-edit-mode ui-icon-button ghost primary"
+                    style="display: none;"
+                    data-ripple="{}"
+                >
+                    ${svg.Edit2}
+                </button>
 
-                    <button
-                        class="app-bar-today ui-icon-button ghost primary"
-                        style="display: none;"
-                        data-ripple="{}"
-                    >
-                    </button>
+                <button
+                    class="app-bar-today ui-icon-button ghost primary"
+                    style="display: none;"
+                    data-ripple="{}"
+                >
+                    ${svg.TodayOutline}
+                </button>
 
-                    <button
-                        class="app-bar-pdf ui-icon-button ghost primary"
-                        style="display: none;"
-                        data-ripple="{}"
-                    >
-                    </button>
+                <button
+                    class="app-bar-pdf ui-icon-button ghost primary"
+                    style="display: none;"
+                    data-ripple="{}"
+                >
+                    ${svg.PDFDocument}
+                </button>
 
-                    <button
-                        class="app-bar-settings ui-icon-button ghost primary"
-                        style="display: none;"
-                        data-ripple="{}"
-                    >
-                    </button>
+                <button
+                    class="app-bar-settings ui-icon-button ghost primary"
+                    style="display: none;"
+                    data-ripple="{}"
+                >
+                    ${svg.Settings}
+                </button>
             </div>
         </div>
     </header>
-`
+`;
 
 async function main() {
-    createRipple()
+  createRipple();
 
-    const themeHandler = await createThemeHandler()
-    themeHandler.start()
+  const themeHandler = await createThemeHandler();
+  themeHandler.start();
 
-    const datePicker = new DatePicker(new Date())
+  const datePicker = new DatePicker(new Date());
 
-    const appBar = new AppBar(
-        document.querySelector(".ui-app-bar"),
-        datePicker,
-    )
+  const appBar = new AppBar(document.querySelector(".ui-app-bar"), datePicker);
 
-    const stackLayout = new StackLayout(
-        document.querySelector("main.container > .stack-layout"),
-        appBar,
-    )
+  const stackLayout = new StackLayout(
+    document.querySelector("main.container > .stack-layout"),
+    appBar,
+  );
 
-    // TODO: ...
+  // TODO: ...
 }
 
-window.addEventListener("DOMContentLoaded", main)
+window.addEventListener("DOMContentLoaded", main);
 
 async function createRipple() {
-    const elements = document.querySelectorAll("*[data-ripple]")
-    elements.forEach(async (el) => {
-        ripple.create(el, JSON.parse(el.getAttribute("data-ripple") || "{}"))
-    })
+  const elements = document.querySelectorAll("*[data-ripple]");
+  elements.forEach(async (el) => {
+    ripple.create(el, JSON.parse(el.getAttribute("data-ripple") || "{}"));
+  });
 }
 
 async function createThemeHandler() {
-    const themeHandler = new utils.theme.ThemeHandler()
+  const themeHandler = new utils.theme.ThemeHandler();
 
-    themeHandler.addTheme("zinc", "/themes/zinc.css")
-    themeHandler.loadTheme(constants.theme.name)
+  themeHandler.addTheme("zinc", "/themes/zinc.css");
+  themeHandler.loadTheme(constants.theme.name);
 
-    const theme = storage.get("theme", null)
-    if (theme?.mode) {
-        themeHandler.setMode(theme.mode)
-    }
+  const theme = storage.get("theme", null);
+  if (theme?.mode) {
+    themeHandler.setMode(theme.mode);
+  }
 
-    return themeHandler
+  return themeHandler;
 }
