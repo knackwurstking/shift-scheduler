c1876c534ff45a36d3185d0a3b7f673614aa381e
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-13 22:16:27 +0200

    Build the date array for months

--- pages/calendar/utils.js
+++ pages/calendar/utils.js
@@ -5,11 +5,21 @@
  */
 export async function getMonthArray(month, weekStart) {
   /** @type {import("../../lib/db").DBMonth} */
-  const days = Array(42).fill({ date: month, shift: null, note: "" });
+  const data = [];
 
-  // TODO: Only set the dates here, and the shift rhythm
+  for (let i = 0; i < 42; i++) {
+    data.push({
+      date: new Date(
+        month.getFullYear(),
+        month.getMonth(),
+        i + 1 - getStartDay(month, weekStart),
+      ),
+      shift: null, // TODO: Calc the current shift (rhythm)
+      note: "",
+    });
+  }
 
-  return days;
+  return data;
 }
 
 /**
@@ -23,4 +33,17 @@ export async function fillWithData(db, month, days) {
   // ...
 
   return days;
+}
+
+/**
+ * @param {Date} month
+ * @param {import("../../lib/storage").StorageDataWeekStart} weekStart
+ * @returns {number}
+ */
+function getStartDay(month, weekStart) {
+  // NOTE: if month starts on sunday (0), but the week start is set to monday (1), than set it to 6 (sunday becomes 6)
+  month.setDate(1); // 0-6 Sun-Sat
+  const d = month.getDay();
+  if (weekStart === 0) return d;
+  else if (weekStart === 1) return d === 0 ? 6 : d - 1;
 }
