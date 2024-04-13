208100447cfc231f07f164513a3668a16b3cdc33
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-13 18:06:05 +0200

    Update types to fill the array

--- pages/calendar/utils.js
+++ pages/calendar/utils.js
@@ -1,12 +1,16 @@
 /**
  * @param {Date} date
  * @param {StorageDataWeekStart} weekStart
- * @returns {Promise<{ current: Date, data: null }[]>}
+ * @param {import("../../lib/db").Custom | null} db
+ * @returns {Promise<import("../../lib/db/db-custom").DBCustomData[]>}
  */
-export async function getDays(date, weekStart) {
-  const days = Array(42).fill({ current: date, data: null });
+export async function getDays(date, weekStart, db = null) {
+  /** @type {import("../../lib/db/db-custom").DBCustomData[]} */
+  const days = Array(42).fill({ date: date, shift: null, note: "" });
+  if (!db) return days;
 
   // TODO: Fill days array, only for the current month, ...
+  // ...
 
   return days;
 }
