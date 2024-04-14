eb69d916579b8122fb3ff2adadf15b0bc5734625
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-15 00:28:14 +0200

    Remove unused imports

--- pages/settings/index.js
+++ pages/settings/index.js
@@ -2,7 +2,6 @@ import { component } from "ui";
 import Base from "ui/src/component/base/base";
 import Page, { utils } from "../page";
 import innerHTML from "./inner-html";
-import constants from "../../lib/constants";
 
 export default class SettingsPage extends Page {
   /** @type {import("../../app.js").default}*/
