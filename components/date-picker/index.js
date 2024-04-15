2f46a89ea701397e69dae5a186a648f06e333000
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-15 14:24:34 +0200

    Fix failed removal of event listeners

--- components/date-picker/index.js
+++ components/date-picker/index.js
@@ -9,108 +9,108 @@ import constants from "../../lib/constants";
  */
 
 export default class DatePicker extends events.Events {
-  /** @type {Date} */
-  #date;
+    /** @type {Date} */
+    #date;
 
-  /**
-   */
-  constructor() {
-    super();
-    /** @type {component.button.Button} */
-    this.button;
-    this.#setup();
-  }
+    /**
+     */
+    constructor() {
+        super();
+        /** @type {component.button.Button} */
+        this.button;
+        this.#setup();
+    }
 
-  onMount() {
-    if (constants.debug) console.log("[datepicker] onMount");
+    onMount() {
+        if (constants.debug) console.log("[datepicker] onMount");
 
-    this.button.element.onclick = () => {
-      // TODO: Open date picker dialog
-    };
+        this.button.element.onclick = () => {
+            // TODO: Open date picker dialog
+        };
 
-    return this;
-  }
+        return this;
+    }
 
-  onDestroy() {
-    if (constants.debug) console.log("[datepicker] onDestroy");
+    onDestroy() {
+        if (constants.debug) console.log("[datepicker] onDestroy");
 
-    this.button.element.onclick = null;
+        this.button.element.onclick = null;
 
-    return this;
-  }
+        return this;
+    }
 
-  getDate() {
-    return this.#date;
-  }
+    getDate() {
+        return this.#date;
+    }
 
-  /** @param {Date} date */
-  setDate(date) {
-    this.#date = date;
-    return this.#update();
-  }
+    /** @param {Date} date */
+    setDate(date) {
+        this.#date = date;
+        return this.#update();
+    }
 
-  nextMonth() {
-    this.#date.setMonth(this.#date.getMonth() + 1);
-    this.#update();
+    nextMonth() {
+        this.#date.setMonth(this.#date.getMonth() + 1);
+        this.#update();
 
-    return this;
-  }
+        return this;
+    }
 
-  prevMonth() {
-    this.#date.setMonth(this.#date.getMonth() - 1);
-    this.#update();
+    prevMonth() {
+        this.#date.setMonth(this.#date.getMonth() - 1);
+        this.#update();
 
-    return this;
-  }
+        return this;
+    }
 
-  toMonthString() {
-    return `${this.#date.getFullYear()} / ${(this.#date.getMonth() + 1).toString().padStart(2, "0")}`;
-  }
+    toMonthString() {
+        return `${this.#date.getFullYear()} / ${(this.#date.getMonth() + 1).toString().padStart(2, "0")}`;
+    }
 
-  /**
-   * @param {EventTypes} type
-   * @param {any} data
-   */
-  dispatchWithData(type, data) {
-    super.dispatchWithData(type, data);
-    return this;
-  }
+    /**
+     * @param {EventTypes} type
+     * @param {any} data
+     */
+    dispatchWithData(type, data) {
+        super.dispatchWithData(type, data);
+        return this;
+    }
 
-  /**
-   * @param {EventTypes} type
-   * @param {(data: any) => void|Promise<void>} listener
-   * @returns (() => void) - clean up function
-   */
-  addListener(type, listener) {
-    return super.addListener(type, listener);
-  }
+    /**
+     * @param {EventTypes} type
+     * @param {(data: any) => void|Promise<void>} listener
+     * @returns (() => void) - clean up function
+     */
+    addListener(type, listener) {
+        return super.addListener(type, listener);
+    }
 
-  /**
-   * @param {EventTypes} type
-   * @param {(data: any) => void|Promise<void>} listener
-   */
-  removeListener(type, listener) {
-    super.addListener(type, listener);
-    return this;
-  }
+    /**
+     * @param {EventTypes} type
+     * @param {(data: any) => void|Promise<void>} listener
+     */
+    removeListener(type, listener) {
+        super.removeListener(type, listener);
+        return this;
+    }
 
-  #update() {
-    this.button.innerHTML = this.toMonthString();
-    this.dispatchWithData("datepickerchange", this.#date);
+    #update() {
+        this.button.innerHTML = this.toMonthString();
+        this.dispatchWithData("datepickerchange", this.#date);
 
-    return this;
-  }
+        return this;
+    }
 
-  #setup() {
-    this.button = new component.button.Button({
-      text: "Date Picker",
-      variant: "outline",
-      color: "primary",
-      id: "appBarDatePicker",
-    });
-    this.#date = new Date();
-    this.#update();
+    #setup() {
+        this.button = new component.button.Button({
+            text: "Date Picker",
+            variant: "outline",
+            color: "primary",
+            id: "appBarDatePicker",
+        });
+        this.#date = new Date();
+        this.#update();
 
-    return this;
-  }
+        return this;
+    }
 }
