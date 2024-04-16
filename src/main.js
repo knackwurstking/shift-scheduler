fd168874ec5c971f567df969e3284d5ace813a5f
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-16 21:16:21 +0200

    Hide all old components for now (will be removed soon)

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import "../node_modules/ui/css/main.css";
+import "./styles/main.css";
+
+import constants from "./lib/constants";
+import App from "./app";
+
+window.addEventListener("DOMContentLoaded", () => {
+    const app = new App(document.querySelector("#app")).onMount().run(); // TODO: Run the onDestroy method?
+
+    // Enable debugging borders
+    if (constants.debug) app.getElement().classList.add("is-debug");
+});
