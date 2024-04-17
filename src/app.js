57f632a17d33adc6b1bf94abea8a9a85959299b3
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-17 04:37:31 +0200

    Update comments

--- src/app.js
+++ src/app.js
@@ -23,18 +23,20 @@ export class App {
     onMount() {
         if (constants.debug) console.log("[app] onMount");
 
-        // create the custom database (shifts and notes per day)
+        // Initialize the database, close if already exists
         if (!!this.db) this.db.close();
+
         this.db = new DB(constants.db.name, constants.db.version);
 
-        // lang storage event listener
+        // Storage event: "lang"
         this.#onlang = async (data) => {
             if (constants.debug) console.log(`[Main] storage: "lang"`, data);
             await this.language.setLanguage(data || constants.language);
         };
+
         this.storage.addListener("lang", this.#onlang);
 
-        // set the app language once
+        // Trigger (storage) language event - this will initialy load the language data
         this.storage.dispatchWithData(
             "lang",
             this.storage.get("lang", constants.language),
@@ -46,7 +48,10 @@ export class App {
     onDestroy() {
         if (constants.debug) console.log("[app] onDestroy");
 
+        // Close database
         if (!!this.db) this.db.close();
+
+        // Storage event: "lang"
         this.storage.removeListener("lang", this.#onlang);
 
         return this;
