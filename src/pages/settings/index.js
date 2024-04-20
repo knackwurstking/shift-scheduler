ade33cccd3ff3edadae49eaf5c653140aa888ab2
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-20 12:12:59 +0200

    Basic settings page methods

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -66,7 +66,43 @@ ${templateMisc} <br /> ${templateShifts} <br /> ${templateBackup} <br /> ${templ
 
 // TODO: Settings page is next...
 export class SettingsPage extends ui.wc.StackLayoutPage {
+    /** @type {import("../../app").App | null} */
+    #app = null;
+    #initialized = false;
+
     constructor() {
         super();
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
+    }
+
+    get app() {
+        return this.#app
+    }
+
+    set app(app) {
+        this.#app = app
+
+        if (super.isConnected && !this.#initialized) {
+            this.#initialized = true
+
+            // TODO: connectedCallback shit here
+        }
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        if (!!this.app) {
+            this.app = this.app
+        }
+    }
+
+    disconnectedCallback() {
+        super.connectedCallback();
+        this.#initialized = false;
+
+        if (!!this.app) {
+            // ...
+        }
     }
 }
