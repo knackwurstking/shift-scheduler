27fb3021feebdff5e2a51d63783821959f21d9f8
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-22 18:23:33 +0200

    Formatting and adding some comments...

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -1,6 +1,6 @@
-import { Filesystem, Encoding, Directory } from "@capacitor/filesystem";
+import { Directory, Encoding, Filesystem } from "@capacitor/filesystem";
 import { Share } from "@capacitor/share";
-import ui from "ui"
+import ui from "ui";
 import { constants } from "../../lib";
 import * as utils from "../../utils";
 
@@ -14,48 +14,63 @@ export class SettingsPage extends ui.wc.StackLayoutPage {
 
         // AppBar Section
 
-        this.appBar.title.innerHTML = this.app.language.get("settings", "appBarTitle")
+        this.appBar.title.innerHTML = this.app.language.get(
+            "settings",
+            "appBarTitle",
+        );
 
         // Misc Section
 
-        this.misc.title.innerHTML =
-            this.app.language.get("settings", "miscTitle");
-        this.misc.weekStartPrimary.innerHTML =
-            this.app.language.get("settings", "miscWeekStartPrimary");
-        this.misc.weekStartSecondary.innerHTML =
-            this.misc.theme.innerHTML =
+        this.misc.title.innerHTML = this.app.language.get(
+            "settings",
+            "miscTitle",
+        );
+        this.misc.weekStartPrimary.innerHTML = this.app.language.get(
+            "settings",
+            "miscWeekStartPrimary",
+        );
+        this.misc.weekStartSecondary.innerHTML = this.misc.theme.innerHTML =
             this.app.language.get("settings", "miscWeekStartSecondary");
         this.app.language.get("settings", "miscTheme");
 
         // Backup Section
 
-        this.backup.title.innerHTML =
-            this.app.language.get("settings", "backupTitle");
-        this.backup.importButton.innerHTML =
-            this.app.language.get("settings", "backupImportButton");
-        this.backup.exportButton.innerHTML =
-            this.app.language.get("settings", "backupExportButton");
+        this.backup.title.innerHTML = this.app.language.get(
+            "settings",
+            "backupTitle",
+        );
+        this.backup.importButton.innerHTML = this.app.language.get(
+            "settings",
+            "backupImportButton",
+        );
+        this.backup.exportButton.innerHTML = this.app.language.get(
+            "settings",
+            "backupExportButton",
+        );
 
         // Shifts Section
 
-        this.shifts.title.innerHTML =
-            this.app.language.get("settings", "shiftsTitle");
-        this.shifts.tableHeaderName.innerHTML =
-            this.app.language.get("settings", "shiftsTableHeaderName");
-        this.shifts.tableHeaderShortName.innerHTML =
-            this.app.language.get("settings", "shiftsTableHeaderShortName");
-        this.shifts.addButton.innerHTML =
-            this.app.language.get("settings", "shiftsAddButton");
+        this.shifts.title.innerHTML = this.app.language.get(
+            "settings",
+            "shiftsTitle",
+        );
+        this.shifts.tableHeaderName.innerHTML = this.app.language.get(
+            "settings",
+            "shiftsTableHeaderName",
+        );
+        this.shifts.tableHeaderShortName.innerHTML = this.app.language.get(
+            "settings",
+            "shiftsTableHeaderShortName",
+        );
+        this.shifts.addButton.innerHTML = this.app.language.get(
+            "settings",
+            "shiftsAddButton",
+        );
     };
 
     #onWeekStartChange = (ev) => {
-        this.app.storage.set(
-            "week-start",
-            ev.currentTarget.checked
-                ? 1
-                : 0
-        );
-    }
+        this.app.storage.set("week-start", ev.currentTarget.checked ? 1 : 0);
+    };
 
     /**
      * @param {CustomEvent<import("ui/src/wc/input").SelectOption>} ev
@@ -63,19 +78,15 @@ export class SettingsPage extends ui.wc.StackLayoutPage {
     #onThemeModeSelectChange = (ev) => {
         utils.setTheme(
             {
-                ...this.app.storage.get(
-                    "theme",
-                    constants.theme
-                ),
-                mode: ev.detail.value
+                ...this.app.storage.get("theme", constants.theme),
+                mode: ev.detail.value,
             },
-            this.app
+            this.app,
         );
     };
 
     #onBackupImport = () => this.importBackup();
     #onBackupExport = () => this.exportBackup();
-
 
     constructor() {
         super();
@@ -101,16 +112,18 @@ export class SettingsPage extends ui.wc.StackLayoutPage {
             importButton: this.querySelector("#backupImportButton"),
             /** @type {import("ui/src/wc/button").Button} */
             exportButton: this.querySelector("#backupExportButton"),
-        }
+        };
 
         this.shifts = {
             title: this.querySelector("#shiftsTitle"),
             /** @type {HTMLTableElement} */
             tableHeaderName: this.querySelector("#shiftsTableHeaderName"),
-            tableHeaderShortName: this.querySelector("#shiftsTableHeaderShortName"),
+            tableHeaderShortName: this.querySelector(
+                "#shiftsTableHeaderShortName",
+            ),
             tableBody: this.querySelector("#shiftsTableBody"),
             addButton: this.querySelector("#shiftsAddButton"),
-        }
+        };
     }
 
     get app() {
@@ -128,14 +141,14 @@ export class SettingsPage extends ui.wc.StackLayoutPage {
             this.#onLang();
 
             // Select current active theme
-            [...this.misc.themeModeSelect.children].forEach(c => {
+            [...this.misc.themeModeSelect.children].forEach((c) => {
                 const theme = this.app.storage.get("theme", constants.theme);
                 if (!!theme) {
                     // @ts-ignore
                     if (c.value === theme.mode) {
-                        c.setAttribute("selected", "")
+                        c.setAttribute("selected", "");
                     } else {
-                        c.removeAttribute("selected")
+                        c.removeAttribute("selected");
                     }
                 }
             });
@@ -146,17 +159,29 @@ export class SettingsPage extends ui.wc.StackLayoutPage {
             );
 
             // Handle week-start change
-            this.misc.weekStartInput.addEventListener("click", this.#onWeekStartChange)
+            this.misc.weekStartInput.addEventListener(
+                "click",
+                this.#onWeekStartChange,
+            );
 
             // Handle theme change
-            this.misc.themeModeSelect.addEventListener("change", this.#onThemeModeSelectChange);
+            this.misc.themeModeSelect.addEventListener(
+                "change",
+                this.#onThemeModeSelectChange,
+            );
 
             // Handle backup import/export
-            this.backup.importButton.addEventListener("click", this.#onBackupImport);
-            this.backup.exportButton.addEventListener("click", this.#onBackupExport);
+            this.backup.importButton.addEventListener(
+                "click",
+                this.#onBackupImport,
+            );
+            this.backup.exportButton.addEventListener(
+                "click",
+                this.#onBackupExport,
+            );
 
             // Handle the shifts table
-            this.#createShiftsTable()
+            this.#createShiftsTable();
         }
     }
 
@@ -174,10 +199,22 @@ export class SettingsPage extends ui.wc.StackLayoutPage {
 
         if (!!this.app) {
             this.#app.storage.removeListener("lang", this.#onLang);
-            this.misc.weekStartInput.removeEventListener("click", this.#onWeekStartChange)
-            this.misc.themeModeSelect.removeEventListener("change", this.#onThemeModeSelectChange);
-            this.backup.importButton.removeEventListener("click", this.#onBackupImport);
-            this.backup.exportButton.removeEventListener("click", this.#onBackupExport);
+            this.misc.weekStartInput.removeEventListener(
+                "click",
+                this.#onWeekStartChange,
+            );
+            this.misc.themeModeSelect.removeEventListener(
+                "change",
+                this.#onThemeModeSelectChange,
+            );
+            this.backup.importButton.removeEventListener(
+                "click",
+                this.#onBackupImport,
+            );
+            this.backup.exportButton.removeEventListener(
+                "click",
+                this.#onBackupExport,
+            );
         }
     }
 
@@ -209,7 +246,8 @@ export class SettingsPage extends ui.wc.StackLayoutPage {
 
     #createShiftsTable() {
         const tbody = this.shifts.tableBody;
-        const settings = this.app.storage.get("settings", constants.Settings)
+        /** @type {import("../../types").Settings} */
+        const settings = this.app.storage.get("settings", constants.Settings);
 
         // TODO: render body data... (get "shifts-settings" from the storage)
         // ...
@@ -230,13 +268,15 @@ export class SettingsPage extends ui.wc.StackLayoutPage {
                         if (!this.#validateSettings(data.settings))
                             throw `invalid settings`;
 
-                        this.app.storage.set("settings", data.settings)
+                        this.app.storage.set("settings", data.settings);
                     }
 
                     // Handle indexedDB - validate all entries
-                    for (const entry of (data.indexedDB || [])) {
+                    for (const entry of data.indexedDB || []) {
                         if (!this.app.db.validate(entry)) {
-                            alert(`Data validation failed for:\n${JSON.stringify(entry, null, 4)}`);
+                            alert(
+                                `Data validation failed for:\n${JSON.stringify(entry, null, 4)}`,
+                            );
                             return;
                         }
 
@@ -246,9 +286,12 @@ export class SettingsPage extends ui.wc.StackLayoutPage {
                     // Handle indexedDB - add/put to database
                     //await this.app.db.deleteAll() // TODO: merge or delete all indexedDB data?
                     let y, m, entry;
-                    for (entry of (data.indexedDB || [])) {
-                        [y, m] = entry.id.split("/", 2).map(n => parseInt(n, 10))
-                        this.app.db.add(y, m, entry.data)
+                    for (entry of data.indexedDB || []) {
+                        [y, m] = entry.id
+                            .split("/", 2)
+                            .map((n) => parseInt(n, 10));
+                        this.app.db
+                            .add(y, m, entry.data)
                             .catch(() => this.app.db.put(y, m, entry.data));
                     }
 
@@ -268,7 +311,10 @@ export class SettingsPage extends ui.wc.StackLayoutPage {
      * @param {import("../../types").Settings} settings
      */
     #validateSettings(settings) {
-        if (!Array.isArray(settings?.shifts) || !Array.isArray(settings?.rhythm)) {
+        if (
+            !Array.isArray(settings?.shifts) ||
+            !Array.isArray(settings?.rhythm)
+        ) {
             return false;
         }
 
@@ -303,19 +349,21 @@ export class SettingsPage extends ui.wc.StackLayoutPage {
      * @param {import("../../types").Backup} data
      */
     async #androidExport(data) {
-        const fileName = await this.#getBackupFileName()
+        const fileName = await this.#getBackupFileName();
         const today = new Date();
         const pM = (today.getMonth() + 1).toString().padStart(2, "0");
         const pD = today.getDate().toString().padStart(2, "0");
 
         Share.share({
             title: `${today.getFullYear()}-${pM}-${pD} Backup`,
-            url: (await Filesystem.writeFile({
-                path: fileName,
-                data: JSON.stringify(data),
-                encoding: Encoding.UTF8,
-                directory: Directory.Cache,
-            })).uri,
+            url: (
+                await Filesystem.writeFile({
+                    path: fileName,
+                    data: JSON.stringify(data),
+                    encoding: Encoding.UTF8,
+                    directory: Directory.Cache,
+                })
+            ).uri,
             dialogTitle: `Backup "${fileName}"`,
         });
     }
