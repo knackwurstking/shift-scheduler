a7444e1081ec84c3f52c25d3db9a3958d3ee43b0
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-26 04:03:50 +0200

    Clean up

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import "./styles.css";
 
 import ui from "ui";
 import { App } from "./app";
-import { constants } from "./lib";
-import { CalendarPage, SettingsPage, StartDate, EditRhythm } from "./pages";
+import { CalendarPage, EditRhythm, SettingsPage, StartDate } from "./pages";
+
+// Initialize Web Components {{{
+
+ui.defineSVG()
+    .catch((err) => alert(`Rendering SVG web components failed: ${err}`));
 
 ui.define()
     .then(() => {
@@ -14,45 +18,56 @@ ui.define()
     })
     .catch((err) => alert(`Rendering web components failed: ${err}`));
 
-window.addEventListener("DOMContentLoaded", () => {
-    /** @type {import("ui/src/wc").Store} */
-    const store = document.querySelector("ui-store");
+// }}}
 
-    store.data.set("date-picker", (new Date()).toString(), true);
-    store.data.set("theme", constants.theme, true);
-    store.data.set("week-start", constants.weekStart, true);
-    store.data.set("settings", constants.settings, true);
-    store.data.set("debug", false, true);
+// Initialize Store {{{
 
-    /** @type {import("ui/src/wc").Lang} */
-    const lang = document.querySelector("ui-lang")
+/** @type {import("ui/src/wc").Store} */
+const store = document.querySelector("ui-store");
 
-    // TODO: Auto language detection handler here...
-    // ...
+store.data.set("date-picker", (new Date()).toString(), true);
+store.data.set("theme", { mode: "system", name: "zinc" }, true);
+store.data.set("week-start", 0, true);
+store.data.set("settings", { shifts: [], rhythm: [], startDate: "" }, true);
+store.data.set("debug", false, true);
 
-    lang.data.on("change", (lt) => {
-        if (!lt) {
-            console.log("[main] No language set for now!")
-            return
-        }
+store.data.on("debug", (/**@type{import("./types").DebugStore}*/state) => {
+    if (state) {
+        document.querySelector("#app").classList.add("is-debug")
+    } else {
+        document.querySelector("#app").classList.remove("is-debug")
+    }
+}, true)
 
-        console.log(`[main] current language in use:`, lt)
-        store.data.set("lang", lt.name, true);
-    }, true)
+// }}}
 
-    store.data.on("debug", (/**@type{import("./types").DebugStore}*/state) => {
-        if (state) {
-            document.querySelector("#app").classList.add("is-debug")
-        } else {
-            document.querySelector("#app").classList.remove("is-debug")
-        }
-    }, true)
+// {{{ Initialize Language
 
+/** @type {import("ui/src/wc").Lang} */
+const lang = document.querySelector("ui-lang")
 
-    /** @type {import("ui/src/wc").ThemeHandler} */
-    const th = document.querySelector("#themeHandler")
-    th.addTheme("zinc", `/themes/zinc.css`);
-    th.addTheme("green", `/themes/green.css`);
+// TODO: Auto language detection handler here...
+// ...
 
-    new App(store).run();
-});
+lang.data.on("change", (lt) => {
+    if (!lt) {
+        console.log("[main] No language set for now!")
+        return
+    }
+
+    console.log(`[main] current language in use:`, lt)
+    store.data.set("lang", lt.name, true);
+}, true)
+
+// }}}
+
+// {{{ Initialize Theme
+
+/** @type {import("ui/src/wc").ThemeHandler} */
+const th = document.querySelector("#themeHandler")
+th.addTheme("zinc", `/themes/zinc.css`);
+th.addTheme("green", `/themes/green.css`);
+
+// }}}
+
+new App(store).run();
