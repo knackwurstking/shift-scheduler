e9a2e7f63a9d8608d2b637a77b4ba12cb0fede94
Udo Bauer <knackwurstking@Udos-Air.fritz.box>
2024-05-11 16:21:18 +0200

    Android back button handler and some code clean up

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import { App as CapApp } from "@capacitor/app";
 import ui from "ui";
 import utils from "./utils";
 
@@ -33,8 +34,33 @@ export default class App extends ui.js.events.Events {
         /** @type {ThemeHandler} */
         this.themeHandler = document.querySelector("#themeHandler");
 
-        // {{{ Stack Layout Initialization
+        this.registerPages()
+        this.createAppBar()
+        this.initializeAndroidHandlers()
+    } // }}}
 
+    run() { // {{{
+        setTimeout(() => {
+            this.stackLayout.ui.events.on(
+                "change",
+                this.onStackLayoutChange.bind(this),
+            );
+
+            this.#store.ui.on("date-picker", (dateString) => {
+                const date = new Date(dateString);
+                this.appBarDatePickerButton.innerText =
+                    `${date.getFullYear()} / ${(date.getMonth() + 1).toString().padStart(2, "0")}`;
+            }, true);
+
+            this.stackLayout.ui.setPage("calendar");
+            document.body.style.display = "block"
+        });
+    } // }}}
+
+    /**
+     * @private
+     */
+    registerPages() { // {{{
         /** @type {StackLayout} */
         this.stackLayout = document.querySelector("ui-stack-layout");
 
@@ -55,17 +81,18 @@ export default class App extends ui.js.events.Events {
                 // @ts-expect-error
                 .content.cloneNode(true)
         });
+    } // }}}
 
-        // }}}
-
-        // {{{ App Bar Initialization 
-
+    /**
+     * @private
+     */
+    createAppBar() { // {{{
         /** @type {AppBar} */
         this.appBar = document.querySelector("ui-app-bar");
 
         /** @type {IconButton} */
         this.appBarBackButton = this.appBar.querySelector("#appBarBackButton")
-        this.appBarBackButton.onclick = async () => this.stackLayout.ui.goBack();
+        this.appBarBackButton.onclick = this.onBack.bind(this);
 
         /** @type {Button} */
         this.appBarDatePickerButton = this.appBar.querySelector("#appBarDatePickerButton")
@@ -86,34 +113,26 @@ export default class App extends ui.js.events.Events {
         /** @type {IconButton} */
         this.appBarSettingsButton = this.appBar.querySelector("#appBarSettingsButton")
         this.appBarSettingsButton.onclick = async () => this.stackLayout.ui.setPage("settings");
-
-        // }}}
-    } // }}}
-
-    run() { // {{{
-        setTimeout(() => {
-            this.stackLayout.ui.events.on(
-                "change",
-                this._onStackLayoutChange.bind(this),
-            );
-
-            this.#store.ui.on("date-picker", (dateString) => {
-                const date = new Date(dateString);
-                this.appBarDatePickerButton.innerText =
-                    `${date.getFullYear()} / ${(date.getMonth() + 1).toString().padStart(2, "0")}`;
-            }, true);
-
-            this.stackLayout.ui.setPage("calendar");
-            document.body.style.display = "block"
-        });
     } // }}}
 
     /**
+     * Android Handlers
+     *  - "backButton"
+     *
+     * @private
+     */
+    initializeAndroidHandlers() { // {{{
+        if (utils.isAndroid()) return;
+        CapApp.addListener("backButton", this.onBack.bind(this))
+    } // }}}
+
+    /**
+     * @private
      * @param {Object} data
      * @param {StackLayoutPage | null} data.newPage 
      * @param {StackLayoutPage | null} data.oldPage 
      */
-    async _onStackLayoutChange({ newPage, oldPage }) { // {{{
+    async onStackLayoutChange({ newPage, oldPage }) { // {{{
         console.debug(`[app] stack layout changed:`, { newPage, oldPage });
 
         // Update the AppBar buttons...
@@ -135,57 +154,49 @@ export default class App extends ui.js.events.Events {
 
         if (!newPage) {
             utils.setAppBarTitle("")
-            this._noPageSetup();
+            this.appBar.removeChild(this.appBarDatePickerButton.parentElement)
+            this.appBar.removeChild(this.appBarEditButton.parentElement)
+            this.appBar.removeChild(this.appBarTodayButton.parentElement)
+            this.appBar.removeChild(this.appBarPDFButton.parentElement)
+            this.appBar.removeChild(this.appBarSettingsButton.parentElement)
             return
         }
 
         switch (newPage.ui.name) {
             case "calendar":
                 utils.setAppBarTitle("")
-                this._calendarPageSetup();
+                this.appBar.appendChild(this.appBarDatePickerButton.parentElement)
+                this.appBar.appendChild(this.appBarEditButton.parentElement)
+                this.appBar.appendChild(this.appBarTodayButton.parentElement)
+                this.appBar.appendChild(this.appBarPDFButton.parentElement)
+                this.appBar.appendChild(this.appBarSettingsButton.parentElement)
                 break;
             case "settings":
                 utils.setAppBarTitle(this.#lang.ui.get("settings", "appBarTitle"))
-                this._settingsPageSetup();
+                this.appBar.removeChild(this.appBarDatePickerButton.parentElement)
+                this.appBar.removeChild(this.appBarEditButton.parentElement)
+                this.appBar.removeChild(this.appBarTodayButton.parentElement)
+                this.appBar.removeChild(this.appBarPDFButton.parentElement)
+                this.appBar.removeChild(this.appBarSettingsButton.parentElement)
                 break;
             case "pdf":
                 utils.setAppBarTitle("@TODO: PDF Page")
-                this._pdfPageSetup();
+                this.appBar.removeChild(this.appBarDatePickerButton.parentElement)
+                this.appBar.removeChild(this.appBarEditButton.parentElement)
+                this.appBar.removeChild(this.appBarTodayButton.parentElement)
+                this.appBar.removeChild(this.appBarPDFButton.parentElement)
+                this.appBar.removeChild(this.appBarSettingsButton.parentElement)
                 break;
             default:
                 throw `unknown page "${newPage.ui.name}"`;
         }
     } // }}}
 
-    _noPageSetup() { // {{{
-        this.appBar.removeChild(this.appBarDatePickerButton.parentElement)
-        this.appBar.removeChild(this.appBarEditButton.parentElement)
-        this.appBar.removeChild(this.appBarTodayButton.parentElement)
-        this.appBar.removeChild(this.appBarPDFButton.parentElement)
-        this.appBar.removeChild(this.appBarSettingsButton.parentElement)
-    } // }}}
-
-    _calendarPageSetup() { // {{{
-        this.appBar.appendChild(this.appBarDatePickerButton.parentElement)
-        this.appBar.appendChild(this.appBarEditButton.parentElement)
-        this.appBar.appendChild(this.appBarTodayButton.parentElement)
-        this.appBar.appendChild(this.appBarPDFButton.parentElement)
-        this.appBar.appendChild(this.appBarSettingsButton.parentElement)
-    } // }}}
-
-    _settingsPageSetup() { // {{{
-        this.appBar.removeChild(this.appBarDatePickerButton.parentElement)
-        this.appBar.removeChild(this.appBarEditButton.parentElement)
-        this.appBar.removeChild(this.appBarTodayButton.parentElement)
-        this.appBar.removeChild(this.appBarPDFButton.parentElement)
-        this.appBar.removeChild(this.appBarSettingsButton.parentElement)
-    } // }}}
-
-    _pdfPageSetup() { // {{{
-        this.appBar.removeChild(this.appBarDatePickerButton.parentElement)
-        this.appBar.removeChild(this.appBarEditButton.parentElement)
-        this.appBar.removeChild(this.appBarTodayButton.parentElement)
-        this.appBar.removeChild(this.appBarPDFButton.parentElement)
-        this.appBar.removeChild(this.appBarSettingsButton.parentElement)
+    /**
+     * @private
+     */
+    async onBack() { // {{{
+        if (!this.appBarBackButton.parentElement) return;
+        this.stackLayout.ui.goBack()
     } // }}}
 }
