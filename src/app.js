f1e4b1d68332a8c8f1e030e7ff7031197579c887
Udo Bauer <knackwurstking@Udos-Air.fritz.box>
2024-05-11 17:15:39 +0200

    Add a FIXME

--- src/app.js
+++ src/app.js
@@ -197,8 +197,8 @@ export default class App extends ui.js.events.Events {
      */
     async onBack() { // {{{
         if (this.stackLayout.ui.stack.length <= 1) return;
-        if (!!document.body.querySelector("ui-dialog")?.hasAttribute("open")) {
-            this.stackLayout.ui.goBack();
-        }
+        // FIXME: Need some lock method for disabling
+        //        the back button handling if a dialog is open
+        this.stackLayout.ui.goBack();
     } // }}}
 } // }}}
