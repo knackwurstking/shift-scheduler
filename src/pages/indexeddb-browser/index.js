92a8431b957588278bee5df4b27d6247862da293
Udo Bauer <knackwurstking@Udos-Air.fritz.box>
2024-05-31 14:32:03 +0200

    Add store an lang objects

--- src/pages/indexeddb-browser/index.js
+++ src/pages/indexeddb-browser/index.js
@@ -1,22 +1,44 @@
 import ui from "ui";
 
+/**
+ * @typedef {import("../../types").StoreEvents} StoreEvents
+ * @typedef {import("ui/src/wc").Store<StoreEvents>} Store
+ * @typedef {import("ui/src/wc").Lang} Lang
+ */
+
 const t = document.createElement("template");
 t.innerHTML = `
 `;
 
 export class IndexedDBBrowserPage extends ui.wc.StackLayoutPage {
+    /** @type {Store} */
+    #store
+    /** @type {Lang} */
+    #lang
+
     static register = () =>
         customElements.define("indexeddb-browser-page", IndexedDBBrowserPage);
 
     constructor() {
         super();
         this.shadowRoot.appendChild(t.content.cloneNode(true));
+
+        this.#store = document.querySelector("ui-store");
+        this.#lang = document.querySelector("ui-lang");
     }
 
     connectedCallback() {
         super.connectedCallback();
 
-        // TODO: Just testing here
-        this.cleanup.add(() => console.warn("cleanup indexeddb-browser page..."))
+        this.cleanup.add(
+            this.#store.ui.on("lang", this.onLang.bind(this), true)
+        );
+    }
+
+    /**
+     * @private
+     */
+    onLang() {
+        // TODO: ...
     }
 }
