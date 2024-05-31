21cf7822d99bc922af6b368f2a2d6e04e71d7a0e
Udo Bauer <knackwurstking@Udos-Air.fritz.box>
2024-05-31 18:19:11 +0200

    Some first version of a database browser

--- src/pages/indexeddb-browser/index.js
+++ src/pages/indexeddb-browser/index.js
@@ -1,4 +1,6 @@
 import ui from "ui";
+import db from "../../db";
+import { html } from "../../utils";
 
 /**
  * @typedef {import("../../types").StoreEvents} StoreEvents
@@ -7,7 +9,17 @@ import ui from "ui";
  */
 
 const t = document.createElement("template");
-t.innerHTML = `
+t.innerHTML = html`
+    <style>
+        :host {
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            padding-top: var(--ui-app-bar-height);
+        }
+    </style>
+
+    <slot></slot>
 `;
 
 export class IndexedDBBrowserPage extends ui.wc.StackLayoutPage {
@@ -25,6 +37,11 @@ export class IndexedDBBrowserPage extends ui.wc.StackLayoutPage {
 
         this.#store = document.querySelector("ui-store");
         this.#lang = document.querySelector("ui-lang");
+
+        this.classList.add("no-scrollbar");
+        this.grid = new ui.wc.FlexGrid();
+        this.grid.setAttribute("gap", "0.25rem");
+        this.appendChild(this.grid);
     }
 
     connectedCallback() {
@@ -33,6 +50,82 @@ export class IndexedDBBrowserPage extends ui.wc.StackLayoutPage {
         this.cleanup.add(
             this.#store.ui.on("lang", this.onLang.bind(this), true)
         );
+
+        this.renderEntries()
+    }
+
+    /**
+     * @private
+     */
+    async renderEntries() {
+        (await db.getAll()).forEach((entry) => {
+            setTimeout(() => {
+                const row = new ui.wc.FlexGridRow();
+                row.setAttribute("gap", "0.25rem");
+
+                { // Year
+                    const item = new ui.wc.FlexGridItem();
+                    item.setAttribute("flex", "0");
+
+                    const content = new ui.wc.Primary();
+                    content.innerHTML = `${entry.year}`;
+                    item.appendChild(content);
+
+                    row.appendChild(item);
+                }
+
+                // Month
+                {
+                    const item = new ui.wc.FlexGridItem();
+                    item.setAttribute("flex", "0");
+
+                    const content = new ui.wc.Primary();
+                    content.innerHTML = entry.month.toString().padStart(2, "0");
+                    item.appendChild(content);
+
+                    row.appendChild(item);
+                }
+
+                // Date
+                {
+                    const item = new ui.wc.FlexGridItem();
+                    item.setAttribute("flex", "0");
+
+                    const content = new ui.wc.Primary();
+                    content.innerHTML = entry.date.toString().padStart(2, "0");
+                    item.appendChild(content);
+
+                    row.appendChild(item);
+                }
+
+                // Shift
+                {
+                    const item = new ui.wc.FlexGridItem();
+                    item.setAttribute("flex", "0");
+
+                    const content = document.createElement("pre");
+                    content.innerText = JSON.stringify(entry.shift, null, 4);
+                    item.appendChild(content);
+
+                    row.appendChild(item);
+                }
+
+                // Note
+                {
+                    const item = new ui.wc.FlexGridItem();
+
+                    const content = new ui.wc.Secondary();
+                    content.innerHTML = `${entry.note}`;
+                    item.appendChild(content);
+
+                    row.appendChild(item);
+                }
+
+                // TODO: Action Buttons: "Delete"
+
+                this.grid.appendChild(row);
+            });
+        });
     }
 
     /**
