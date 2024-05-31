6d714fefc3f127a9fb0fd94baa79f7146acee4c7
Udo Bauer <knackwurstking@Udos-Air.fritz.box>
2024-05-31 20:01:43 +0200

    Set min width of 100%

--- src/pages/indexeddb-browser/index.js
+++ src/pages/indexeddb-browser/index.js
@@ -45,6 +45,7 @@ export class IndexedDBBrowserPage extends ui.wc.StackLayoutPage {
 
         this.grid = new ui.wc.FlexGrid();
         this.grid.setAttribute("gap", "0.25rem");
+        this.grid.style.minWidth = "100%";
         this.grid.style.width = "fit-content";
         this.appendChild(this.grid);
     }
