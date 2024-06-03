89592328b6b673b9a13bd65438fd06d1b42c79c2
Udo Bauer <knackwurstking@Udos-Air.fritz.box>
2024-06-03 13:06:02 +0200

    Add minify vite plugin and remove user-scalable opt from viewport

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 export default defineConfig({
-    plugins: [],
+    plugins: [
+        ViteMinifyPlugin({}),
+    ],
     clearScreen: false,
 });
