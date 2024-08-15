efe0c4c95c81eba400d2ce4bc285d8b85a6102dc
Udo Bauer <knackwurstking.tux@gmail.com>
2024-08-15 23:45:06 +0200

    Update `ShiftAddButton` component

--- src/components/pages/settings/index.js
+++ src/components/pages/settings/index.js
@@ -1,6 +1,5 @@
-import { html } from "ui";
-import { EditRhythm, IndexedDBBrowser } from "./sections";
-import { UIStackLayoutPage } from "ui";
+import { html, UIStackLayoutPage } from "ui";
+import { EditRhythm, IndexedDBBrowser, ShiftAddButton } from "./sections";
 
 // {{{ HTML content
 const content = html`
@@ -73,109 +72,108 @@ const content = html`
 // }}}
 
 export class SettingsPage extends UIStackLayoutPage {
-    static register = () => {
-        EditRhythm.register();
-        IndexedDBBrowser.register();
+  static register = () => {
+    EditRhythm.register();
+    IndexedDBBrowser.register();
+    ShiftAddButton.register();
 
-        customElements.define("settings-page", SettingsPage);
-    };
+    customElements.define("settings-page", SettingsPage);
+  };
 
-    constructor() {
-        // {{{
-        super();
+  constructor() {
+    // {{{
+    super();
 
-        /** @type {import("ui").UIStore<UIStoreEvents>} */
-        this.uiStore;
-        /** @type {import("ui").UILang} */
-        this.uiLang;
+    /** @type {import("ui").UIStore<UIStoreEvents>} */
+    this.uiStore;
+    /** @type {import("ui").UILang} */
+    this.uiLang;
 
-        this.innerHTML = content;
+    this.innerHTML = content;
 
-        this.ui.name = "settings";
+    this.ui.name = "settings";
 
-        this.#store = document.querySelector("ui-store");
-        this.#lang = document.querySelector("ui-lang");
+    this.#store = document.querySelector("ui-store");
+    this.#lang = document.querySelector("ui-lang");
 
-        this.createMiscElements();
-        this.createShiftElements();
-        this.createIndexedDBElements();
-    } // }}}
+    this.createMiscElements();
+    this.createShiftElements();
+    this.createIndexedDBElements();
+  } // }}}
 
-    connectedCallback() {
-        // {{{
-        super.connectedCallback();
+  connectedCallback() {
+    // {{{
+    super.connectedCallback();
 
-        setTimeout(() => {
-            this.cleanup.add(
-                this.#store.ui.on("lang", this.onLang.bind(this), true),
-            );
-        });
-    } // }}}
+    setTimeout(() => {
+      this.cleanup.add(this.#store.ui.on("lang", this.onLang.bind(this), true));
+    });
+  } // }}}
 
-    /** @private */
-    createMiscElements() {
-        // {{{
-        this.querySelector("#miscWeekStartSection").appendChild(
-            new WeekStart(this.#store, this.#lang),
-        );
+  /** @private */
+  createMiscElements() {
+    // {{{
+    this.querySelector("#miscWeekStartSection").appendChild(
+      new WeekStart(this.#store, this.#lang),
+    );
 
-        this.querySelector("#miscThemePickerSection").appendChild(
-            new ThemePicker(this.#store, this.#lang),
-        );
-    } // }}}
+    this.querySelector("#miscThemePickerSection").appendChild(
+      new ThemePicker(this.#store, this.#lang),
+    );
+  } // }}}
 
-    /** @private */
-    createShiftElements() {
-        // {{{
-        this.querySelector("#shiftsStartDateSection").appendChild(
-            new StartDate(this.#store, this.#lang),
-        );
+  /** @private */
+  createShiftElements() {
+    // {{{
+    this.querySelector("#shiftsStartDateSection").appendChild(
+      new StartDate(this.#store, this.#lang),
+    );
 
-        this.querySelector("#shiftsEditRhythmSection").appendChild(
-            new EditRhythm(this.#store, this.#lang),
-        );
+    this.querySelector("#shiftsEditRhythmSection").appendChild(
+      new EditRhythm(this.#store, this.#lang),
+    );
 
-        this.querySelector("#shiftsTable").appendChild(
-            new ShiftsTable(this.#store, this.#lang),
-        );
+    this.querySelector("#shiftsTable").appendChild(
+      new ShiftsTable(this.#store, this.#lang),
+    );
 
-        this.querySelector("#shiftsAddSection").appendChild(
-            new ShiftAddButton(this.#store, this.#lang),
-        );
+    this.querySelector("#shiftsAddSection").appendChild(
+      new ShiftAddButton(this.#store, this.#lang),
+    );
 
-        this.querySelector("#shiftsBackupSection").appendChild(
-            new ShiftsBackup(this.#store, this.#lang),
-        );
-    } // }}}
+    this.querySelector("#shiftsBackupSection").appendChild(
+      new ShiftsBackup(this.#store, this.#lang),
+    );
+  } // }}}
 
-    /** @private */
-    createIndexedDBElements() {
-        // {{{
-        this.querySelector("#indexedDBBrowserSection").appendChild(
-            new IndexedDBBrowser(this.#store, this.#lang),
-        );
-    } // }}}
+  /** @private */
+  createIndexedDBElements() {
+    // {{{
+    this.querySelector("#indexedDBBrowserSection").appendChild(
+      new IndexedDBBrowser(this.#store, this.#lang),
+    );
+  } // }}}
 
-    /** @private */
-    async onLang() {
-        // {{{
-        // Misc Title
+  /** @private */
+  async onLang() {
+    // {{{
+    // Misc Title
 
-        this.querySelector("#miscTitle").innerHTML = this.#lang.ui.get(
-            "settings",
-            "title-misc",
-        );
+    this.querySelector("#miscTitle").innerHTML = this.#lang.ui.get(
+      "settings",
+      "title-misc",
+    );
 
-        // Shifts Title
+    // Shifts Title
 
-        this.querySelector("#shiftsTitle").innerHTML = this.#lang.ui.get(
-            "settings",
-            "title-shifts",
-        );
+    this.querySelector("#shiftsTitle").innerHTML = this.#lang.ui.get(
+      "settings",
+      "title-shifts",
+    );
 
-        this.querySelector("#indexedDBTitle").innerHTML = this.#lang.ui.get(
-            "settings",
-            "title-indexedDB",
-        );
-    } // }}}
+    this.querySelector("#indexedDBTitle").innerHTML = this.#lang.ui.get(
+      "settings",
+      "title-indexedDB",
+    );
+  } // }}}
 }
--- src/components/pages/settings/sections/shift-add-button.js
+++ src/components/pages/settings/sections/shift-add-button.js
@@ -1,122 +1,104 @@
-import { UIButton } from "ui";
-import { CleanUp } from "ui/src/js";
+import { CleanUp, UIButton } from "ui";
 import { EditShiftDialog } from "../../../dialogs";
 
-/**
- * @typedef {import("../../../types/.index").UIStoreEvents} UIStoreEvents
- * @typedef {import("../../../types/.index").Shift} Shift
- */
-
 export class ShiftAddButton extends UIButton {
-  static register = () => {
-    UIButton.register();
-
-    EditShiftDialog.register();
-
-    customElements.define("settings-shift-add-button", ShiftAddButton);
-  };
-
-  /**
-   * @param {import("ui").UIStore<UIStoreEvents>} store
-   * @param {import("ui").UILang} lang
-   */
-  constructor(store, lang) {
-    // {{{
-    super();
-    super.setAttribute("color", "primary");
-    super.setAttribute("variant", "full");
-
-    /** @type {import("ui").UIStore<UIStoreEvents>} */
-    this.uiStore = store;
-    /** @type {import("ui").UILang} */
-    this.uiLang = lang;
-
-    this.cleanup = new CleanUp();
-  } // }}}
-
-  connectedCallback() {
-    // {{{
-    super.connectedCallback();
-    this.handleEvents();
-
-    this.cleanup.add(this.uiStore.ui.on("lang", this.onLang.bind(this), true));
-  } // }}}
-
-  disconnectedCallback() {
-    this.cleanup.run();
-  }
-
-  /** @private */
-  handleEvents() {
-    // {{{
-    this.cleanup.add(this.ui.events.on("click", this.onClick.bind(this)));
-  } // }}}
-
-  /**
-   * @private
-   * @param {EditShiftDialog} dialog
-   */
-  onDialogClose(dialog) {
-    document.body.removeChild(dialog);
-  }
-
-  /** @private */
-  onClick() {
-    // {{{
-    /** @type {Shift} */
-    let shift = {
-      id: new Date().getTime(),
-      name: "",
-      shortName: "",
-      visible: true,
-      color: null,
+    static register = () => {
+        customElements.define("settings-shift-add-button", ShiftAddButton);
     };
 
-    const dialog = new EditShiftDialog(shift, this.uiStore, this.uiLang);
-    document.body.appendChild(dialog);
+    constructor() {
+        super();
 
-    dialog.ui.open(true);
-    dialog.ui.events.on("close", () => this.onDialogClose(dialog));
+        this.cleanup = new CleanUp();
 
-    /**
-     * @param {Shift} newShift
-     */
-    const onSubmit = async (newShift) => {
-      // {{{
-      // If shift name is missing then reopen the dialog
-      if (!newShift.name) {
-        alert(this.uiLang.ui.get("edit-shift-alerts", "missing-name"));
+        /** @type {SchedulerStore} */
+        this.uiStore = document.querySelector("ui-store");
+        /** @type {import("ui").UILang} */
+        this.uiLang = document.querySelector("ui-lang");
 
-        shift = newShift;
+        this.render();
+    }
 
-        const dialog = new EditShiftDialog(shift, this.uiStore, this.uiLang);
-        document.body.appendChild(dialog);
+    render() {
+        this.ui.variant = "full";
+        this.ui.color = "primary";
+    }
 
-        dialog.ui.open(true);
-        dialog.ui.events.on("close", () => this.onDialogClose(dialog));
+    connectedCallback() {
+        super.connectedCallback();
+
+        this.cleanup.add(
+            this.ui.events.on("click", this.clickHandler.bind(this)),
+            this.uiStore.ui.on("lang", this.storeHandlerLang.bind(this), true),
+        );
+    }
+
+    disconnectedCallback() {
+        this.cleanup.run();
+    }
+
+    /** @private */
+    clickHandler() {
+        /**
+         * @param {Shift} newShift
+         */
+        const onSubmit = async (newShift) => {
+            // If shift name is missing then reopen the dialog
+            if (!newShift.name) {
+                alert(this.uiLang.ui.get("edit-shift-alerts", "missing-name"));
+
+                shift = newShift;
+
+                const dialog = new EditShiftDialog();
+                dialog.set(shift);
+
+                dialog.ui.events.on("close", () => {
+                    document.body.removeChild(dialog);
+                });
+
+                dialog.ui.events.on("submit", onSubmit);
+
+                document.body.appendChild(dialog);
+                dialog.ui.open(true);
+                return;
+            }
+
+            if (!newShift.shortName) {
+                newShift.shortName = newShift.name.slice(0, 2);
+            }
+
+            this.uiStore.ui.update("settings", (settings) => {
+                return {
+                    ...settings,
+                    shifts: [...settings.shifts, newShift],
+                };
+            });
+        };
+
+        /** @type {Shift} */
+        let shift = {
+            id: new Date().getTime(),
+            name: "",
+            shortName: "",
+            visible: true,
+            color: null,
+        };
+
+        const dialog = new EditShiftDialog();
+        dialog.set(shift);
+
+        dialog.ui.events.on("close", () => {
+            document.body.removeChild(dialog);
+        });
+
         dialog.ui.events.on("submit", onSubmit);
 
-        return;
-      }
+        document.body.appendChild(dialog);
+        dialog.ui.open(true);
+    }
 
-      if (!newShift.shortName) {
-        newShift.shortName = newShift.name.slice(0, 2);
-      }
-
-      this.uiStore.ui.update("settings", (settings) => {
-        return {
-          ...settings,
-          shifts: [...settings.shifts, newShift],
-        };
-      });
-    }; // }}}
-
-    dialog.ui.events.on("submit", onSubmit);
-  } // }}}
-
-  /** @private */
-  async onLang() {
-    // {{{
-    this.innerHTML = this.uiLang.ui.get("settings", "button-add-shift");
-  } // }}}
-} // }}}
+    /** @private */
+    async storeHandlerLang() {
+        this.innerHTML = this.uiLang.ui.get("settings", "button-add-shift");
+    }
+}
