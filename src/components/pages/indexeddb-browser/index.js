29e91486c1ddbadcc6044b4a58a0bb069b5b724e
Udo Bauer <knackwurstking.tux@gmail.com>
2024-08-15 12:02:20 +0200

    Update db browser page

--- src/components/pages/indexeddb-browser/index.js
+++ src/components/pages/indexeddb-browser/index.js
@@ -1,187 +1,180 @@
-import {
-  SvgRecycle,
-  UIFlexGrid,
-  UIFlexGridItem,
-  UIIconButton,
-  UIInput,
-  UIStackLayoutPage,
-} from "ui";
-import { html } from "ui/src/js";
-import db from "../../db";
+import { UIStackLayoutPage, html } from "ui";
+import { pages } from "../../../data/constants";
+import { CleanUp } from "ui";
+import { db } from "../../../lib";
+import { UIFlexGridItem } from "ui";
 
 /**
- * @typedef {import("ui/src/ui-input").UIInputEvents} UIInputEvents
- * @typedef {import("../../types/.index").UIStoreEvents} UIStoreEvents
- * @typedef {import("../../types/.index").DBDataEntry} DBDataEntry
+ * @typedef Values
+ * @type {{
+ *   y: string | null;
+ *   m: string | null;
+ *   d: string | null;
+ * }}
  */
 
 const filterHeight = "4rem";
 
-// {{{ HTML Content
-const content = html`
-  <ui-flex-grid
-    style="min-width: 100%; width: fit-content;"
-    class="content"
-    gap="0.5rem"
-  >
-  </ui-flex-grid>
-
-  <div
-    style="
-            position: fixed;
-            right: 0;
-            bottom: 0;
-            left: 0;
-            height: ${filterHeight};
-            border-top: 1px solid var(--ui-borderColor);
-        "
-  >
-    <div
-      style="
-                position: absolute;
-                top: 0;
-                right: 0;
-                bottom: 0;
-                left: 0;
-                background-color: var(--ui-backdrop-bgColor);
-                backdrop-filter: var(--ui-backdropFilter);
-            "
-    ></div>
-
-    <ui-flex-grid>
-      <ui-flex-grid-row>
-        <ui-flex-grid-item>
-          <ui-input title="Year" type="number"></ui-input>
-        </ui-flex-grid-item>
-
-        <ui-flex-grid-item>
-          <ui-input title="Month" type="number"></ui-input>
-        </ui-flex-grid-item>
-
-        <ui-flex-grid-item>
-          <ui-input title="Day" type="number"></ui-input>
-        </ui-flex-grid-item>
-      </ui-flex-grid-row>
-    </ui-flex-grid>
-  </div>
-`;
-// }}}
-
+/**
+ * HTML: `indexeddb-browser-page`
+ */
 export class IndexedDBBrowserPage extends UIStackLayoutPage {
-  static register = () => {
-    SvgRecycle.register();
-
-    UIFlexGrid.register();
-    UIFlexGridItem.register();
-    UIIconButton.register();
-    UIInput.register();
-
-    customElements.define("indexeddb-browser-page", IndexedDBBrowserPage);
-  };
-
-  constructor() {
-    // {{{
-    super();
-    this.innerHTML = content;
-    this.className = "no-scrollbar";
-    this.style.width = "100%";
-    this.style.height = "100%";
-    this.style.overflowY = "auto";
-    this.style.paddingTop = "var(--ui-app-bar-height)";
-    this.style.paddingBottom = `${filterHeight}`;
-
-    this.ui.name = "indexeddb-browser";
-
-    /** @type {import("ui").UIStore<UIStoreEvents>} */
-    this.uiStore = document.querySelector("ui-store");
-    /** @type {import("ui").UILang} */
-    this.uiLang = document.querySelector("ui-lang");
-
-    this.createSearchBar();
-  } // }}}
-
-  connectedCallback() {
-    // {{{
-    super.connectedCallback();
-
-    this.cleanup.add(this.uiStore.ui.on("lang", this.onLang.bind(this), true));
-
-    this.renderEntries();
-  } // }}}
-
-  /**
-   * @param {Object} values
-   * @param {number | string | null} values.y
-   * @param {number | string | null} values.m
-   * @param {number | string | null} values.d
-   */
-  filter({ y, m, d }) {
-    // {{{
-    if (y === null) y = ".*";
-    if (m === null) m = ".*";
-    if (d === null) d = ".*";
-
-    /**
-     * @type {UIFlexGridItem[]}
-     */
-    // @ts-expect-error
-    const children = [...this.children];
-    children.forEach((child) => {
-      if (child.getAttribute("key").match(new RegExp(`${y}-${m}-${d}`))) {
-        child.style.display = "block";
-      } else {
-        child.style.display = "none";
-      }
-    });
-  } // }}}
-
-  /**
-   * @private
-   */
-  createSearchBar() {
-    // {{{
-    const [y, m, d] = this.getInputs();
-
-    let values = {
-      y: null,
-      m: null,
-      d: null,
+    static register = () => {
+        customElements.define("indexeddb-browser-page", IndexedDBBrowserPage);
     };
 
-    y.ui.events.on("input", (/** @type {number} */ value) => {
-      values.y = isNaN(value) ? null : value;
-      this.filter(values);
-    });
+    constructor() {
+        super(pages.dbBrowser);
 
-    m.ui.events.on("input", (/** @type {number} */ value) => {
-      values.m = isNaN(value) ? null : value;
-      this.filter(values);
-    });
+        this.cleanup = new CleanUp();
 
-    d.ui.events.on("input", (/** @type {number} */ value) => {
-      values.d = isNaN(value) ? null : value;
-      this.filter(values);
-    });
-  } // }}}
+        /** @type {SchedulerStore} */
+        this.uiStore = document.querySelector("ui-store");
+        /** @type {import("ui").UILang} */
+        this.uiLang = document.querySelector("ui-lang");
 
-  /**
-   * @private
-   */
-  async renderEntries() {
-    // {{{
-    const settings = this.uiStore.ui.get("settings");
-    const entries = await db.getAll();
-    const content = this.querySelector("ui-flex-grid.content");
-    entries.forEach((entry) => {
-      setTimeout(() => {
-        const item = new UIFlexGridItem();
+        this.render();
+    }
 
-        const y = entry.year;
-        const m = entry.month + 1;
-        const d = entry.date;
-        item.setAttribute("key", `${y}-${m}-${d}`);
+    render() {
+        this.className = "no-scrollbar";
 
-        item.innerHTML = `
+        this.style.width = "100%";
+        this.style.height = "100%";
+        this.style.overflowY = "auto";
+        this.style.paddingTop = "var(--ui-app-bar-height)";
+        this.style.paddingBottom = `${filterHeight}`;
+
+        this.innerHTML = html`
+            <ui-flex-grid
+                style="min-width: 100%; width: fit-content;"
+                class="content"
+                gap="0.5rem"
+            >
+            </ui-flex-grid>
+
+            <div
+                style="
+                    position: fixed;
+                    right: 0;
+                    bottom: 0;
+                    left: 0;
+                    height: ${filterHeight};
+                    border-top: 1px solid var(--ui-borderColor);
+                "
+            >
+                <div
+                    style="
+                        position: absolute;
+                        top: 0;
+                        right: 0;
+                        bottom: 0;
+                        left: 0;
+                        background-color: var(--ui-backdrop-bgColor);
+                        backdrop-filter: var(--ui-backdropFilter);
+                    "
+                ></div>
+
+                <ui-flex-grid>
+                    <ui-flex-grid-row>
+                        <ui-flex-grid-item>
+                            <ui-input title="Year" type="number"></ui-input>
+                        </ui-flex-grid-item>
+
+                        <ui-flex-grid-item>
+                            <ui-input title="Month" type="number"></ui-input>
+                        </ui-flex-grid-item>
+
+                        <ui-flex-grid-item>
+                            <ui-input title="Day" type="number"></ui-input>
+                        </ui-flex-grid-item>
+                    </ui-flex-grid-row>
+                </ui-flex-grid>
+            </div>
+        `;
+
+        // Initialize filter bar
+        const [y, m, d] = this.getInputs();
+
+        /** @type {Values} */
+        let values = { y: null, m: null, d: null };
+
+        y.ui.events.on("input", (value) => {
+            values.y = value || null;
+            this.filter(values);
+        });
+
+        m.ui.events.on("input", (value) => {
+            values.m = value || null;
+            this.filter(values);
+        });
+
+        d.ui.events.on("input", (value) => {
+            values.m = value || null;
+            this.filter(values);
+        });
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        this.cleanup.add(
+            this.uiStore.ui.on("lang", this.storeHandlerLang.bind(this), true),
+        );
+
+        this.renderEntries();
+    }
+
+    disconnectedCallback() {
+        super.disconnectedCallback();
+        this.cleanup.run();
+    }
+
+    /** @param {Values} values */
+    filter({ y, m, d }) {
+        if (y === null) y = ".*";
+        if (m === null) m = ".*";
+        if (d === null) d = ".*";
+
+        /** @type {UIFlexGridItem[]} */
+        Array.from(this.children).forEach(
+            (/** @type {HTMLElement} */ child) => {
+                if (
+                    child
+                        .getAttribute("key")
+                        .match(new RegExp(`${y}-${m}-${d}`))
+                ) {
+                    child.style.display = "block";
+                } else {
+                    child.style.display = "none";
+                }
+            },
+        );
+    }
+
+    /**
+     * @private
+     * @returns {import("ui").UIInput<import("ui").UIInput_Events>[]}
+     */
+    getInputs() {
+        return Array.from(this.querySelectorAll("ui-input"));
+    }
+
+    /** @private */
+    async renderEntries() {
+        const settings = this.uiStore.ui.get("settings");
+        const entries = await db.getAll();
+        const content = this.querySelector("ui-flex-grid.content");
+        entries.forEach((entry) => {
+            setTimeout(() => {
+                const item = new UIFlexGridItem();
+
+                const y = entry.year;
+                const m = entry.month + 1;
+                const d = entry.date;
+                item.setAttribute("key", `${y}-${m}-${d}`);
+
+                item.innerHTML = html`
                     <table>
                         <tr>
                             <td style="width: 50%;">${y}</td>
@@ -202,32 +195,32 @@ export class IndexedDBBrowserPage extends UIStackLayoutPage {
                     </table>
                 `;
 
-        /** @type {UIIconButton} */
-        const deleteButton = item.querySelector("ui-icon-button.delete");
-        deleteButton.onclick = async () => {
-          const message = this.uiLang.ui
-            .get("indexeddb-browser", "confirm-delete-entry")
-            .replace("%d", entry.year.toString())
-            .replace("%d", entry.month.toString().padStart(2, "0"))
-            .replace("%d", entry.date.toString().padStart(2, "0"));
+                /** @type {import("ui").UIIconButton} */
+                const deleteButton = item.querySelector(
+                    "ui-icon-button.delete",
+                );
 
-          if (window.confirm(message)) {
-            await db.delete(entry.year, entry.month, entry.date);
-            content.removeChild(item);
-          }
-        };
+                deleteButton.onclick = async () => {
+                    const message = this.uiLang.ui
+                        .get("indexeddb-browser", "confirm-delete-entry")
+                        .replace("%d", entry.year.toString())
+                        .replace("%d", entry.month.toString().padStart(2, "0"))
+                        .replace("%d", entry.date.toString().padStart(2, "0"));
 
-        if (!!entry.shift) {
-          entry.shift = settings.shifts.find(
-            (shift) => shift.id === entry.shift.id,
-          );
+                    if (window.confirm(message)) {
+                        await db.delete(entry.year, entry.month, entry.date);
+                        content.removeChild(item);
+                    }
+                };
 
-          const row = document.createElement("tr");
-          row.innerHTML = `
-                        <td
-                            colspan="3"
-                            style="user-select: text;"
-                        >
+                if (!!entry.shift) {
+                    entry.shift = settings.shifts.find(
+                        (shift) => shift.id === entry.shift.id,
+                    );
+
+                    const row = document.createElement("tr");
+                    row.innerHTML = html`
+                        <td colspan="3" style="user-select: text;">
                             ${entry.shift?.name || "&nbsp;"}
                         </td>
 
@@ -237,52 +230,52 @@ export class IndexedDBBrowserPage extends UIStackLayoutPage {
                                 user-select: text;
                             "
                         >
-                            ${
-                              !!entry.shift?.visible
+                            ${!!entry.shift?.visible
                                 ? entry.shift?.shortName || "&nbsp;"
-                                : "&nbsp;"
-                            }
+                                : "&nbsp;"}
                         </td>
                     `;
-          item.querySelector("table").appendChild(row);
-        }
+                    item.querySelector("table").appendChild(row);
+                }
 
-        if (!!entry.note) {
-          const row = document.createElement("tr");
-          row.innerHTML = `
+                if (!!entry.note) {
+                    const row = document.createElement("tr");
+                    row.innerHTML = html`
                         <td colspan="5">
                             <pre
                                 style="white-space: normal; user-select: text;"
                             >
-                                ${entry.note.trim().replaceAll("\n", "<br/>") || "&nbsp;"}
-                            </pre>
+                                ${entry.note.trim().replaceAll("\n", "<br/>") ||
+                                "&nbsp;"}
+                            </pre
+                            >
                         </td>
                     `;
-          item.querySelector("table").appendChild(row);
-        }
+                    item.querySelector("table").appendChild(row);
+                }
 
-        content.appendChild(item);
-      });
-    });
-  } // }}}
+                content.appendChild(item);
+            });
+        });
+    }
 
-  /**
-   * @private
-   * @returns {NodeListOf<UIInput<UIInputEvents, "number">>}
-   */
-  getInputs() {
-    // {{{
-    return this.querySelectorAll("ui-input");
-  } // }}}
+    /** @private */
+    storeHandlerLang() {
+        const [y, m, d] = this.getInputs();
 
-  /**
-   * @private
-   */
-  onLang() {
-    // {{{
-    const [y, m, d] = this.getInputs();
-    y.ui.title = this.uiLang.ui.get("indexeddb-browser", "input-title-year");
-    m.ui.title = this.uiLang.ui.get("indexeddb-browser", "input-title-month");
-    d.ui.title = this.uiLang.ui.get("indexeddb-browser", "input-title-date");
-  } // }}}
+        y.ui.title = this.uiLang.ui.get(
+            "indexeddb-browser",
+            "input-title-year",
+        );
+
+        m.ui.title = this.uiLang.ui.get(
+            "indexeddb-browser",
+            "input-title-month",
+        );
+
+        d.ui.title = this.uiLang.ui.get(
+            "indexeddb-browser",
+            "input-title-date",
+        );
+    }
 }
