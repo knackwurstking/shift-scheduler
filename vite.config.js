35ddaafc326c6996ba7cdf6e65dbe7e217ae387b
Udo Bauer <knackwurstking.tux@gmail.com>
2025-02-17 00:05:52 +0100

    docs: add todo

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
+// TODO: Add icons, and manifest configuration
+
 export default defineConfig({
     plugins: [VitePWA({ registerType: "prompt" })],
 });
