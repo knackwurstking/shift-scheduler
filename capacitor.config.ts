60980f454fcceccd344fc7a4a22198b1025e42a4
Udo Bauer <knackwurstking.tux@gmail.com>
2025-03-04 19:40:39 +0100

    chore: add android platform for capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'knackwurstking.shiftscheduler',
+  appName: 'shift-scheduler',
+  webDir: 'dist'
+};
+
+export default config;
