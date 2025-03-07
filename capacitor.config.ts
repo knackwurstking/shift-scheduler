e01b8227712fe89cefe046001f9d9b8f562727e2
Udo Bauer <knackwurstking.tux@gmail.com>
2025-03-07 02:38:51 +0100

    style: formatting

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'knackwurstking.shiftscheduler',
-  appName: 'shift-scheduler',
-  webDir: 'dist'
+    appId: "knackwurstking.shiftscheduler",
+    appName: "shift-scheduler",
+    webDir: "../shift-scheduler.github.io/",
 };
 
 export default config;
