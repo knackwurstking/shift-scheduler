5924331e724ca6e6e0a8b2aae2da66e13a8d06e2
Udo Bauer <knackwurstking.tux@gmail.com>
2025-03-11 02:29:44 +0100

    wip: add create function

--- src/pages/calendar/dialogs/calendar-print.ts
+++ src/pages/calendar/dialogs/calendar-print.ts
@@ -1,6 +1,22 @@
-// TODO: Add create function here, just like the date-picker dialog
+import { DialogCreate } from "@types";
+import { html } from "@utils";
+
+export function create(): DialogCreate {
+    const dialog = document.createElement("dialog");
+
+    dialog.innerHTML = html``; // TODO: ...
+
+    return {
+        dialog,
+        destroy() {
+            document.body.removeChild(this.dialog);
+        },
+    };
+}
+
 export function open(): Promise<void> {
     return new Promise((resolve) => {
+        //const { dialog, destroy } = create();
         // TODO: Continue here
     });
 }
