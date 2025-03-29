bea2c57f0768ff1c814bae0a8a36cbac513daa05
Udo Bauer <knackwurstking.tux@gmail.com>
2025-03-29 16:18:30 +0100

    refactor: clean up routes and layouts

--- src/routes/(calendar)/+layout.svelte
+++ src/routes/(calendar)/+layout.svelte
@@ -7,7 +7,6 @@
 		<button
 			class="date-picker"
 			style="
-                display: none;
                 margin: 0.25rem;
                 height: calc(100% - 0.5rem);
                 text-wrap: nowrap;
@@ -16,46 +15,23 @@
 		>
 			YYYY / MM
 		</button>
-
-		<button
-			class="back"
-			style="display: none"
-			data-variant="ghost"
-			color="primary"
-			data-icon
-			aria-label="back"
-			onclick={() => history.back()}
-		>
-			<div class="bi bi-chevron-left"></div>
-		</button>
-	</div>
-
-	<div class="center">
-		<h4 class="title ui-ellipsis">Shift Scheduler</h4>
 	</div>
 
 	<div class="right">
-		<button class="edit" style="display: none" data-variant="ghost" data-icon aria-label="edit">
+		<button class="edit" data-variant="ghost" data-icon aria-label="edit">
 			<i class="bi bi-pen"></i>
 		</button>
 
-		<button class="today" style="display: none" data-variant="ghost" data-icon aria-label="today">
+		<button class="today" data-variant="ghost" data-icon aria-label="today">
 			<i class="bi bi-calendar-event"></i>
 		</button>
 
-		<button
-			class="printer"
-			style="display: none"
-			data-variant="ghost"
-			data-icon
-			aria-label="printer"
-		>
+		<button class="printer" data-variant="ghost" data-icon aria-label="printer">
 			<i class="bi bi-printer"></i>
 		</button>
 
 		<button
 			class="settings"
-			style="display: none"
 			data-variant="ghost"
 			data-icon
 			aria-label="settings"
