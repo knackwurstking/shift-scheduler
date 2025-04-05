9568e2dcb177306c6f1e027d5ae7d2bf231bb7d1
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-06 00:58:57 +0200

    feat(dbbrowser): render yyyy/mm/dd

--- src/pages/dbbrowser/index.ts
+++ src/pages/dbbrowser/index.ts
@@ -1,7 +1,6 @@
-// TODO: Continue here...
 import * as ui from "ui";
 
-import { appBarUtils, html } from "@lib";
+import { appBarUtils, db, html } from "@lib";
 
 let appBarTitleBackup = "";
 let cleanup: ui.CleanUpFunction[] = [];
@@ -10,7 +9,7 @@ export async function onMount() {
     appBarTitleBackup = appBarUtils.getTitle();
     appBarUtils.setTitle("DB-Browser");
 
-    document.querySelector("#routerTarget")!.innerHTML = getHTML();
+    document.querySelector("#routerTarget")!.innerHTML = await getHTML();
 
     // ...
 }
@@ -21,6 +20,36 @@ export async function onDestroy() {
     cleanup = [];
 }
 
-function getHTML(): string {
-    return html``; // ...
+async function getHTML(): Promise<string> {
+    // TODO: Generate a list with all indexed db entries for each day and a
+    //       search bar to the bottom (or top?)
+    const entryItems: string[] = (await db.getAll()).map((entry) => {
+        return html`
+            <div class="ui-flex-grid ui-debug">
+                <span class="ui-flex-grid-row">
+                    <span class="ui-flex-grid-item" style="text-align: center;">
+                        ${entry.year}
+                    </span>
+
+                    <span class="ui-flex-grid-item" style="text-align: center;">
+                        ${(entry.month + 1).toString().padStart(2, "0")}
+                    </span>
+
+                    <span class="ui-flex-grid-item" style="text-align: center;">
+                        ${entry.date.toString().padStart(2, "0")}
+                    </span>
+                </span>
+
+                <!-- TODO: The shift (short name) if changed (not null) -->
+
+                <!-- TODO: Note if any -->
+            </div>
+        `; // TODO: ...
+    });
+
+    return html`
+        <ul>
+            ${entryItems.join("")}
+        </ul>
+    `;
 }
