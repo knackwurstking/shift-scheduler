93d6388bb244d29009d90c1a218f966785bda92d
Udo Bauer <knackwurstking.tux@gmail.com>
2025-06-09 14:43:33 +0200

    Update docs

---
+++ docs/assets/web-B8VxcpYI.js
@@ -0,0 +1,1 @@
+import{W as e}from"./index-Dfl0heYU.js";class r extends e{constructor(){super()}async checkSendIntentReceived(){return{title:""}}finish(){}}export{r as SendIntentWeb};
--- docs/assets/web-CzOU33Dj.js
+++
@@ -1,1 +0,0 @@
-import{W as e}from"./index-qj7OVsJp.js";class r extends e{constructor(){super()}async checkSendIntentReceived(){return{title:""}}finish(){}}export{r as SendIntentWeb};
